3634e4d356c32bed144b17478db69915a9dda8d6
tech-scripts <techscriptsgithub@gmail.com>
2025-04-05 22:17:19 +0500

    Update lang.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

--- misc/tech/lang.sh
+++ misc/tech/lang.sh
@@ -2,7 +2,6 @@
 
 SUDO=$(command -v sudo)
 
-# Создание директории и файла конфигурации, если они не существуют
 if [ ! -d "/etc/tech-scripts" ]; then
     $SUDO mkdir -p /etc/tech-scripts
 fi
@@ -11,19 +10,16 @@ if [ ! -f "/etc/tech-scripts/choose.conf" ]; then
     $SUDO touch /etc/tech-scripts/choose.conf
 fi
 
-# Выбор языка
 LANGUAGE=$(dialog --title "Language Selection" --menu "Choose language:" 10 40 2 \
     1 "English" \
     2 "Русский" \
     3>&1 1>&2 2>&3)
 
-# Выход, если выбор отменен
 if [ $? -ne 0 ]; then
     echo "Selection canceled!"
     exit 1
 fi
 
-# Установка выбранного языка
 case $LANGUAGE in
     1)
         lang="English"
@@ -37,8 +33,4 @@ case $LANGUAGE in
         ;;
 esac
 
-# Сохранение выбранного языка в конфигурационный файл
 echo "lang: $lang" | $SUDO tee /etc/tech-scripts/choose.conf > /dev/null
-
-# Вывод сообщения об успешном завершении
-echo "Language set to: $lang"
