b68cf5ec307b6189233ec3c4c8aa15345abdffd3
tech-scripts <techscriptsgithub@gmail.com>
2025-04-05 10:52:08 +0500

    Update timeout.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

--- misc/grub/timeout.sh
+++ misc/grub/timeout.sh
@@ -1,38 +1,37 @@
 #!/bin/bash
 
 SUDO=$(command -v sudo)
+LANG_CONF=""
+[ -f /etc/tech-scripts/choose.conf ] && LANG_CONF=$(grep '^lang:' /etc/tech-scripts/choose.conf | cut -d':' -f2 | tr -d ' ')
+if [ "$LANG_CONF" = "Русский" ]; then
+    INPUT_MSG="Введите количество секунд для задержки перед запуском:"
+    CANCEL_MSG="Отмена. Скрипт завершен."
+    CONFIRM_PROMPT="Вы ввели: %s секунд. Это правильно?"
+    ERROR_MSG="Ошибка: Введите целое число."
+    SUCCESS_MSG="Задержка перед запуском установлена на %s секунд. Конфигурация GRUB обновлена."
+else
+    INPUT_MSG="Enter the number of seconds for the boot delay:"
+    CANCEL_MSG="Cancelled. Script terminated."
+    CONFIRM_PROMPT="You entered: %s seconds. Is this correct?"
+    ERROR_MSG="Error: Please enter a whole number."
+    SUCCESS_MSG="Boot delay set to %s seconds. GRUB configuration updated."
+fi
 
 is_number() {
     [[ $1 =~ ^[0-9]+$ ]]
 }
 
-LANG=$(grep 'lang:' /etc/tech-scripts/choose.conf | awk '{print $2}')
-
-if [[ $LANG == "Русский" ]]; then
-    MSG_INPUT="Введите количество секунд для задержки перед запуском:"
-    MSG_CANCEL="Отмена. Скрипт завершен."
-    MSG_CONFIRM="Вы ввели: %d секунд. Это правильно?"
-    MSG_ERROR="Ошибка: Введите целое число."
-    MSG_SUCCESS="Задержка перед запуском установлена на %d секунд. Конфигурация GRUB обновлена."
-else
-    MSG_INPUT="Enter the number of seconds to delay before startup:"
-    MSG_CANCEL="Cancelled. Script terminated."
-    MSG_CONFIRM="You entered: %d seconds. Is this correct?"
-    MSG_ERROR="Error: Please enter a whole number."
-    MSG_SUCCESS="Startup delay set to %d seconds. GRUB configuration updated."
-fi
-
 while true; do
-    delay=$(dialog --stdout --inputbox "$MSG_INPUT" 0 0)
-    [ $? -ne 0 ] && echo "$MSG_CANCEL" && exit 1
+    delay=$(dialog --stdout --inputbox "$INPUT_MSG" 0 0)
+    [ $? -ne 0 ] && echo "$CANCEL_MSG" && exit 1
     if is_number "$delay"; then
-        dialog --yesno "$(printf "$MSG_CONFIRM" "$delay")" 6 40
+        dialog --yesno "$(printf "$CONFIRM_PROMPT" "$delay")" 6 40
         [ $? -eq 0 ] && break
     else
-        dialog --msgbox "$MSG_ERROR" 6 40
+        dialog --msgbox "$ERROR_MSG" 6 40
     fi
 done
 
 $SUDO sed -i "s/GRUB_TIMEOUT=.*/GRUB_TIMEOUT=$delay/" /etc/default/grub
 $SUDO update-grub
-dialog --msgbox "$(printf "$MSG_SUCCESS" "$delay")" 8 50
+dialog --msgbox "$(printf "$SUCCESS_MSG" "$delay")" 8 50
