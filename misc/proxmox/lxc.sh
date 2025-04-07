4fc1205bc4ff83690aea6d488174cb0d289cdda0
tech-scripts <techscriptsgithub@gmail.com>
2025-04-07 17:54:19 +0500

    Update lxc.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

--- misc/proxmox/lxc.sh
+++ misc/proxmox/lxc.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-LANG_CONF=$(grep '^lang:' /etc/tech-scripts/choose.conf 2>/dev/null | cut -d':' -f2 | tr -d ' ')
 CONFIG_FILE="/etc/tech-scripts/choose.conf"
-EDITOR=$(grep '^editor:' /etc/tech-scripts/choose.conf | cut -d ' ' -f 2)
-
-if [ -f "$CONFIG_FILE" ]; then
-    LANG_CONF=$(grep '^lang:' "$CONFIG_FILE" | cut -d':' -f2 | tr -d ' ')
-else
-    LANG_CONF="English"
-fi
+LANG_CONF=$(grep '^lang:' "$CONFIG_FILE" 2>/dev/null | cut -d':' -f2 | tr -d ' ')
+EDITOR=$(grep '^editor:' "$CONFIG_FILE" | cut -d ' ' -f 2)
 
 if [ "$LANG_CONF" = "Русский" ]; then
     DIALOG_NOT_FOUND="Утилита dialog не установлена. Установите её с помощью команды: sudo apt install dialog"
@@ -19,7 +13,6 @@ if [ "$LANG_CONF" = "Русский" ]; then
     MSG_CONFIRM_DELETE="Вы уверены, что хотите удалить"
     MSG_SUCCESS="Успешно выполнено"
     MSG_ERROR="Ошибка"
-    MSG_LOCK="Контейнер заблокирован"
     MSG_UNLOCK="Контейнер разблокирован"
     CONTINUE="Продолжить?"
 else
@@ -31,41 +24,32 @@ else
     MSG_CONFIRM_DELETE="Are you sure you want to delete"
     MSG_SUCCESS="Successfully executed"
     MSG_ERROR="Error"
-    MSG_LOCK="Container locked"
     MSG_UNLOCK="Container unlocked"
     CONTINUE="Continue?"
 fi
 
-# Проверка наличия утилиты dialog
 if ! command -v dialog &> /dev/null; then
     echo "$DIALOG_NOT_FOUND"
     exit 1
 fi
 
-# Проверка наличия утилиты pct
 if ! command -v pct &> /dev/null; then
     echo "$PCT_NOT_FOUND"
     exit 1
 fi
 
-# Основной цикл меню действий
 while true; do
-    # Получение списка контейнеров
     containers=$(pct list | awk 'NR>1 {print $1, $3}')
-
-    # Проверка наличия контейнеров
     if [ -z "$containers" ]; then
         dialog --msgbox "$NO_CONTAINERS" 5 40
         exit 1
     fi
 
-    # Формирование списка опций для выбора контейнера
     options=()
     while read -r container_id container_name; do
         options+=("$container_id" "$container_name")
     done <<< "$containers"
 
-    # Основное меню действий
     while true; do
         selected_container_id=$(dialog --title "$SELECT_CONTAINER" --menu "$SELECT_CONTAINER:" 15 50 10 "${options[@]}" 3>&1 1>&2 2>&3)
         if [ $? != 0 ]; then
@@ -79,7 +63,7 @@ while true; do
                 2 "Выключить" \
                 3 "Перезагрузить" \
                 4 "Открыть конфигурационный файл" \
-                5 "Уничтожить" \
+                5 "Удалить" \
                 6 "Разблокировать" \
                 7 "Усыпить" \
                 8 "Разбудить" \
@@ -104,48 +88,25 @@ while true; do
             exit
         fi
 
-        # Обработка выбранного действия
         case $ACTION in
-            1)
-                pct start "$selected_container_id" && dialog --msgbox "$MSG_SUCCESS" 5 30 || dialog --msgbox "$MSG_ERROR" 5 30
-                ;;
-            2)
-                pct stop "$selected_container_id" && dialog --msgbox "$MSG_SUCCESS" 5 30 || dialog --msgbox "$MSG_ERROR" 5 30
-                ;;
-            3)
-                pct reboot "$selected_container_id" && dialog --msgbox "$MSG_SUCCESS" 5 30 || dialog --msgbox "$MSG_ERROR" 5 30
-                ;;
-            4)
-                $EDITOR "/etc/pve/lxc/$selected_container_id.conf"
-                ;;
+            1) pct start "$selected_container_id" && dialog --msgbox "$MSG_SUCCESS" 5 30 || dialog --msgbox "$MSG_ERROR" 5 30 ;;
+            2) pct stop "$selected_container_id" && dialog --msgbox "$MSG_SUCCESS" 5 30 || dialog --msgbox "$MSG_ERROR" 5 30 ;;
+            3) pct reboot "$selected_container_id" && dialog --msgbox "$MSG_SUCCESS" 5 30 || dialog --msgbox "$MSG_ERROR" 5 30 ;;
+            4) $EDITOR "/etc/pve/lxc/$selected_container_id.conf" ;;
             5)
                 if dialog --yesno "$MSG_CONFIRM_DELETE $selected_container_id?" 7 60; then
                     pct stop "$selected_container_id"
                     pct destroy "$selected_container_id" && dialog --msgbox "$MSG_SUCCESS" 5 30 || dialog --msgbox "$MSG_ERROR" 5 30
                 fi
                 ;;
-            6)
-                pct unlock "$selected_container_id" && dialog --msgbox "$MSG_LOCK" 5 30 || dialog --msgbox "$MSG_ERROR" 5 30
-                ;;
-            7)
-                pct suspend "$selected_container_id" && dialog --msgbox "$MSG_UNLOCK" 5 30 || dialog --msgbox "$MSG_ERROR" 5 30
-                ;;
-            8)
-                pct resume "$selected_container_id" && dialog --msgbox "$MSG_UNLOCK" 5 30 || dialog --msgbox "$MSG_ERROR" 5 30
-                ;;
-            9)
-                pct console "$selected_container_id" && dialog --msgbox "$MSG_UNLOCK" 5 30 || dialog --msgbox "$MSG_ERROR" 5 30
-                ;;
-            10)
-                clear
-                exit 0
-                ;;
-            *)
-                dialog --msgbox "$MSG_ERROR" 5 30
-                ;;
+            6) pct unlock "$selected_container_id" && dialog --msgbox "$MSG_UNLOCK" 5 30 || dialog --msgbox "$MSG_ERROR" 5 30 ;;
+            7) pct suspend "$selected_container_id" && dialog --msgbox "$MSG_UNLOCK" 5 30 || dialog --msgbox "$MSG_ERROR" 5 30 ;;
+            8) pct resume "$selected_container_id" && dialog --msgbox "$MSG_UNLOCK" 5 30 || dialog --msgbox "$MSG_ERROR" 5 30 ;;
+            9) pct console "$selected_container_id" && dialog --msgbox "$MSG_UNLOCK" 5 30 || dialog --msgbox "$MSG_ERROR" 5 30 ;;
+            10) clear; exit 0 ;;
+            *) dialog --msgbox "$MSG_ERROR" 5 30 ;;
         esac
 
-        # Запрос на продолжение
         if dialog --title "$CONTINUE" --yesno "$CONTINUE" 5 40; then
             continue
         else
