23b4b37febc389e9a511aa804ea79d4f4428cd27
tech-scripts <techscriptsgithub@gmail.com>
2025-04-12 21:27:06 +0500

    Update timeout.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

--- misc/debian/grub/timeout.sh
+++ misc/debian/grub/timeout.sh
@@ -6,13 +6,11 @@ LANG_CONF=""
 
 if [ "$LANG_CONF" = "Русский" ]; then
     INPUT_MSG="Введите количество секунд для задержки перед запуском:"
-    CANCEL_MSG="Отмена. Скрипт завершен."
     CONFIRM_PROMPT="Вы ввели: %s секунд. Это правильно?"
     ERROR_MSG="Ошибка: Введите целое число."
     SUCCESS_MSG="Задержка перед запуском установлена на %s секунд. Конфигурация GRUB обновлена!"
 else
     INPUT_MSG="Enter the number of seconds for the boot delay:"
-    CANCEL_MSG="Cancelled. Script terminated."
     CONFIRM_PROMPT="You entered: %s seconds. Is this correct?"
     ERROR_MSG="Error: Please enter a whole number."
     SUCCESS_MSG="Boot delay set to %s seconds. GRUB configuration updated!"
@@ -24,7 +22,7 @@ is_number() {
 
 while true; do
     delay=$(whiptail --inputbox "$INPUT_MSG" 0 0 3>&1 1>&2 2>&3)
-    [ $? -ne 0 ] && echo "$CANCEL_MSG" && exit 1
+    [ $? -ne 0 ] && exit 1
     if is_number "$delay"; then
         if whiptail --yesno "$(printf "$CONFIRM_PROMPT" "$delay")" 7 40; then
             break
