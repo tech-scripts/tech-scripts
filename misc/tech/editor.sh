ea2de0b25232df3ee5fb7b404da7ba5a323e523b
tech-scripts <techscriptsgithub@gmail.com>
2025-04-15 21:56:28 +0500

    Update editor.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

--- misc/tech/editor.sh
+++ misc/tech/editor.sh
@@ -9,7 +9,6 @@ if [ "$LANG_CONF" = "Русский" ]; then
     MSG_EDITOR="Выберите предпочитаемый текстовый редактор:"
     TITLE_CUSTOM="Пользовательский редактор"
     MSG_CUSTOM="Введите команду для вашего текстового редактора:"
-    MSG_CANCEL="Выбор отменен!"
     MSG_CUSTOM_CANCEL="Ввод пользовательского редактора отменен!"
     MSG_INVALID="Неверный выбор!"
     MSG_SUCCESS="Текстовый редактор установлен:"
@@ -18,7 +17,6 @@ else
     MSG_EDITOR="Choose your preferred text editor:"
     TITLE_CUSTOM="Custom Editor"
     MSG_CUSTOM="Enter the command for your custom text editor:"
-    MSG_CANCEL="Selection canceled!"
     MSG_CUSTOM_CANCEL="Custom editor input canceled!"
     MSG_INVALID="Invalid choice!"
     MSG_SUCCESS="Text editor set to:"
@@ -34,9 +32,6 @@ EDITOR=$(whiptail --title "$TITLE_EDITOR" --menu "$MSG_EDITOR" 12 40 3 \
     3>&1 1>&2 2>&3)
 
 if [ $? -ne 0 ]; then
-    echo " "
-    echo "$MSG_CANCEL"
-    echo " "
     exit 1
 fi
 
