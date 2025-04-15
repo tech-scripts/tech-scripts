c0f7d26a46735b005d48b2dd3000482d9055388e
tech-scripts <techscriptsgithub@gmail.com>
2025-04-15 18:02:32 +0500

    Update disk.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

--- misc/tools/benchmark/disk.sh
+++ misc/tools/benchmark/disk.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+SUDO=$(command -v sudo)
+
 FILE_SIZE="1G"
 LANG_FILE=$(grep 'lang:' /etc/tech-scripts/choose.conf | awk '{print $2}')
 
@@ -39,10 +41,10 @@ done
 selected_disk=$(whiptail --title "$msg_select" --menu "" 15 60 4 "${disk_choices[@]}" 3>&1 1>&2 2>&3)
 
 temp_file="$selected_disk/testfile"
-output=$(dd if=/dev/zero of="$temp_file" bs="$FILE_SIZE" count=1 oflag=direct 2>&1)
+output=$($SUDO dd if=/dev/zero of="$temp_file" bs="$FILE_SIZE" count=1 oflag=direct 2>&1)
 write_time=$(echo "$output" | grep -o '[0-9.]* s' | head -n 1)
 write_speed=$(echo "$output" | grep -o '[0-9.]* [MG]B/s' | head -n 1 || echo "$msg_failed")
-output=$(dd if="$temp_file" of=/dev/null bs="$FILE_SIZE" count=1 iflag=direct 2>&1)
+output=$($SUDO dd if="$temp_file" of=/dev/null bs="$FILE_SIZE" count=1 iflag=direct 2>&1)
 read_time=$(echo "$output" | grep -o '[0-9.]* s' | head -n 1)
 read_speed=$(echo "$output" | grep -o '[0-9.]* [MG]B/s' | head -n 1 || echo "$msg_failed")
 echo ""
@@ -54,4 +56,4 @@ echo ""
 echo "$msg_speed_read $read_speed"
 echo "$msg_time_read $read_time"
 echo ""
-rm -f "$temp_file"
+$SUDO rm -f "$temp_file"
