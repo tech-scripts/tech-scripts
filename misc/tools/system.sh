ab72f6177bff5351b83115703df9220c3fcd8818
tech-scripts <techscriptsgithub@gmail.com>
2025-04-16 19:36:16 +0500

    Update system.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

--- misc/tools/system.sh
+++ misc/tools/system.sh
@@ -9,10 +9,7 @@ show_system_info() {
     MEMORY=$(free -h | grep "Mem:" | awk '{print $2}')
     DISK=$(df -h / | grep "/" | awk '{print $2}')
     IP=$(hostname -I | awk '{print $1}')
-
     MESSAGE="
-Информация о системе:
-
 ОС: $OS
 Ядро: $KERNEL
 Время работы: $UPTIME
@@ -23,7 +20,7 @@ show_system_info() {
 IP-адрес: $IP
 "
 
-    whiptail --title "Информация о системе" --scrolltext --msgbox "$MESSAGE" 20 60
+    whiptail --title "Информация о системе" --scrolltext --msgbox "$MESSAGE" 15 50
 }
 
 show_temperature_info() {
@@ -42,12 +39,10 @@ show_temperature_info() {
     fi
 
     MESSAGE="
-Температура:
-
 $TEMP_INFO
 "
 
-    whiptail --title "Температура" --scrolltext --msgbox "$MESSAGE" 20 60
+    whiptail --title "Температура" --scrolltext --msgbox "$MESSAGE" 15 50
 }
 
 show_disk_info() {
@@ -58,7 +53,7 @@ show_disk_info() {
 $DISK_INFO
 "
 
-    whiptail --title "Информация о дисках" --scrolltext --msgbox "$MESSAGE" 20 60
+    whiptail --title "Информация о дисках" --scrolltext --msgbox "$MESSAGE" 15 50
 }
 
 show_security_info() {
@@ -80,8 +75,6 @@ show_security_info() {
     ACTIVE_USERS=$(who)
 
     MESSAGE="
-Информация о безопасности:
-
 Статус брандмауэра (UFW):
 $UFW_STATUS
 
@@ -93,12 +86,9 @@ $APPARMOR_STATUS
 
 Проверка обновлений безопасности:
 $SECURITY_UPDATES
-
-Активные пользователи:
-$ACTIVE_USERS
 "
 
-    whiptail --title "Информация о безопасности" --scrolltext --msgbox "$MESSAGE" 20 80
+    whiptail --title "Информация о безопасности" --scrolltext --msgbox "$MESSAGE" 15 50
 }
 
 main_menu() {
@@ -107,13 +97,11 @@ main_menu() {
             "1" "Информация о системе" \
             "2" "Температура" \
             "3" "Информация о дисках" \
-            "4" "Безопасность" \
-            "5" "Выход" 3>&1 1>&2 2>&3)
+            "4" "Безопасность" 3>&1 1>&2 2>&3)
 
         case $OPTION in
             1) show_system_info ;;
             2) show_temperature_info ;;
             3) show_disk_info ;;
             4) show_security_info ;;
-            5) exit 0 ;;
-            *) whiptail --title "Ошибка" --msgbox "Неверный выбор. Пожалуйста, попробуйте снова." 8 
+            *) exit 0
