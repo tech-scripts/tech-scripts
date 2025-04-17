86b8b2ff9897e585bcf120520e4bda1f2aaf0bca
tech-scripts <techscriptsgithub@gmail.com>
2025-04-17 21:47:44 +0500

    Update update.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

--- misc/tech/update.sh
+++ misc/tech/update.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+cat << 'EOF' > /tmp/update.sh
+#!/bin/bash
+
 rm -rf /tmp/tech-scripts
-git clone --depth 1 "https://github.com/tech-scripts/linux.git" "/tmp/tech-scripts"
+git clone --depth 1 https://github.com/tech-scripts/linux.git /tmp/tech-scripts
 rm -rf /usr/local/bin/tech
 cd "/tmp/tech-scripts/misc/tech"
 chmod +x tech.sh
@@ -9,3 +12,8 @@ chmod +x tech.sh
 echo ""
 echo "Обновление завершено!"
 echo ""
+EOF
+
+chmod +x /tmp/update.sh
+/tmp/update.sh
+rm -f /tmp/update.sh
