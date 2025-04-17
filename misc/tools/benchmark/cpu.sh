dfb61956da4f3e3aaa4eaaf4fccfb9cfd03e8c0c
tech-scripts <techscriptsgithub@gmail.com>
2025-04-17 19:20:43 +0500

    Update cpu.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

--- misc/tools/benchmark/cpu.sh
+++ misc/tools/benchmark/cpu.sh
@@ -4,21 +4,26 @@ whiptail --title "Стресс-тест процессора" --yesno "Вы хо
 
 if [ $? -eq 0 ]; then
     echo "Запуск теста на одно ядро..."
-    single_core_result=$(sysbench cpu --threads=1 run)
+    single_core_result=$(sysbench cpu --time=5 --threads=1 run)
     echo "Запуск теста на все ядра..."
-    multi_core_result=$(sysbench cpu --threads=$(nproc) run)
-
+    multi_core_result=$(sysbench cpu --time=5 --threads=$(nproc) run)
+    echo ""
+    echo "Single core"
+    echo ""
     echo "CPU speed:"
     echo "$single_core_result" | grep "events per second:"
     echo ""
     echo "General statistics:"
     echo "$single_core_result" | grep -E "total time:|total number of events:"
     echo ""
+    echo "Multi core"
+    echo ""
     echo "CPU speed:"
     echo "$multi_core_result" | grep "events per second:"
     echo ""
     echo "General statistics:"
     echo "$multi_core_result" | grep -E "total time:|total number of events:"
+    echo ""
 else
     echo "Стресс-тест отменен."
 fi
