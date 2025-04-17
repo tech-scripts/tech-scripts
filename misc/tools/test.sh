5297f2c17138bf2b350847af767f7793b1d1e72b
tech-scripts <techscriptsgithub@gmail.com>
2025-04-17 15:41:19 +0500

    Update test.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

--- misc/tools/test.sh
+++ misc/tools/test.sh
@@ -219,7 +219,6 @@ create_settings_menu() {
         [{"text": "Уведомления о закрытых соединениях", "callback_data": "toggle_closed"}]
     ]
 }
-EOF
     )
     send_telegram_menu "$chat_id" "$MSG_SETTINGS" "$keyboard"
 }
