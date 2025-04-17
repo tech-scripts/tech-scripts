a4e1a938afd9b7512ecc662375d3c89f486eb5fb
tech-scripts <techscriptsgithub@gmail.com>
2025-04-17 19:15:51 +0500

    Update cpu.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

--- misc/tools/benchmark/cpu.sh
+++ misc/tools/benchmark/cpu.sh
@@ -8,10 +8,17 @@ if [ $? -eq 0 ]; then
     echo "Запуск теста на все ядра..."
     multi_core_result=$(sysbench cpu --threads=$(nproc) run)
 
-    echo "Single Core Results:"
-    echo "$single_core_result" | grep -E "CPU speed:|General statistics:"
-    echo "Multi Core Results:"
-    echo "$multi_core_result" | grep -E "CPU speed:|General statistics:"
+    echo "CPU speed:"
+    echo "$single_core_result" | grep "events per second:"
+    echo ""
+    echo "General statistics:"
+    echo "$single_core_result" | grep -E "total time:|total number of events:"
+    echo ""
+    echo "CPU speed:"
+    echo "$multi_core_result" | grep "events per second:"
+    echo ""
+    echo "General statistics:"
+    echo "$multi_core_result" | grep -E "total time:|total number of events:"
 else
     echo "Стресс-тест отменен."
 fi
