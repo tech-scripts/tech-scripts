2f43e5e6025ef33502be0a018745ae0ffb63df1f
tech-scripts <techscriptsgithub@gmail.com>
2025-04-17 19:28:36 +0500

    Update cpu.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

--- misc/tools/benchmark/cpu.sh
+++ misc/tools/benchmark/cpu.sh
@@ -2,7 +2,7 @@
 
 whiptail --title "Стресс-тест процессора" --yesno "Вы хотите выполнить стресс-тест процессора?" 10 60
 
-if [ $? -eq 0 ]; then
+if [ $?q 0 ]; then
     echo "Запуск теста на одно ядро..."
     single_core_result=$(sysbench cpu --time=5 --threads=1 run)
     echo "Запуск теста на все ядра..."
@@ -11,18 +11,18 @@ if [ $? -eq 0 ]; then
     echo "Single core"
     echo ""
     echo "CPU speed:"
-    echo "$single_core_result" | grep "events per second:"
+    echo "\e[37m$(echo "$single_core_result" | grep "events per second:")\e[0m"
     echo ""
     echo "General statistics:"
-    echo "$single_core_result" | grep -E "total time:|total number of events:"
+    echo "\e[37m$(echo "$single_core_result" | grep -E "total time:|total number of events:")\e[0m"
     echo ""
     echo "Multi core"
     echo ""
     echo "CPU speed:"
-    echo "$multi_core_result" | grep "events per second:"
+    echo "\e[37m$(echo "$multi_core_result" | grep "events per second:")\e[0m"
     echo ""
     echo "General statistics:"
-    echo "$multi_core_result" | grep -E "total time:|total number of events:"
+    echo "\e[37m$(echo "$multi_core_result" | grep -E "total time:|total number of events:")\e[0m"
     echo ""
 else
     echo "Стресс-тест отменен."
