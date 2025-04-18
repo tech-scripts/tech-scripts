15fa175d0dbaf982c62501ce7b79fc4028cc29ca
tech-scripts <techscriptsgithub@gmail.com>
2025-04-18 12:10:03 +0500

    Update update.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

--- misc/tech/update.sh
+++ misc/tech/update.sh
@@ -3,12 +3,13 @@
 cat << 'EOF' > /tmp/update.sh
 #!/bin/bash
 
+SUDO=$(command -v sudo)
 LANGUAGE=$(grep '^lang:' /etc/tech-scripts/choose.conf | cut -d' ' -f2)
 
 cd /tmp
 rm -rf /tmp/tech-scripts
 git clone --depth 1 https://github.com/tech-scripts/linux.git /tmp/tech-scripts
-rm -rf /usr/local/bin/tech
+$SUDO rm -rf /usr/local/bin/tech
 cd "/tmp/tech-scripts/misc/tech"
 chmod +x tech.sh
 ./tech.sh
