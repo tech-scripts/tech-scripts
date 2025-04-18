131e8fd96dba16e096b6f48d5ad43d76dae6af21
tech-scripts <techscriptsgithub@gmail.com>
2025-04-18 10:35:21 +0500

    Update localization.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

--- misc/localization.sh
+++ misc/localization.sh
@@ -11,6 +11,19 @@ if [[ "$LANGUAGE" == "Русский" ]]; then
     DIRECTORY_FORMAT="директория"
     SCRIPT_FORMAT="скрипт"
     OPTION_FORMAT="опция"
+
+    QM_NOT_FOUND="Утилита qm не найдена. Убедитесь, что Proxmox установлен!"
+    NO_VMS="Нет доступных виртуальных машин!"
+    SELECT_VM="Выберите виртуальную машину"
+    SELECT_ACTION="Выберите действие"
+    MSG_CONFIRM_DELETE="Вы уверены, что хотите удалить"
+    MSG_ERROR="Ошибка"
+    CONTINUE_VM="Продолжить работу с текущей виртуальной машиной?"
+
+    PCT_NOT_FOUND="Утилита pct не найдена. Убедитесь, что Proxmox установлен!"
+    NO_CONTAINERS_LXC="Нет доступных LXC-контейнеров!"
+    SELECT_CONTAINER="Выберите контейнер"
+    CONTINUE_LXC="Продолжить работу с текущим контейнером?"
 else
     MSG_NO_SCRIPTS="No available scripts or directories!"
     MSG_CANCELLED="Selection cancelled!"
@@ -20,4 +33,17 @@ else
     DIRECTORY_FORMAT="directory"
     SCRIPT_FORMAT="script"
     OPTION_FORMAT="option"
+
+    QM_NOT_FOUND="Utility qm not found. Make sure Proxmox is installed!"
+    NO_VMS="No available virtual machines!"
+    SELECT_VM="Select virtual machine"
+    SELECT_ACTION="Select action"
+    MSG_CONFIRM_DELETE="Are you sure you want to delete"
+    MSG_ERROR="Error"
+    CONTINUE_VM="Continue working with the current virtual machine?"
+
+    PCT_NOT_FOUND="Utility pct not found. Make sure Proxmox is installed!"
+    NO_CONTAINERS_LXC="No available LXC containers!"
+    SELECT_CONTAINER="Select container"
+    CONTINUE_LXC="Continue working with the current container?"
 fi
