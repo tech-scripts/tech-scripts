2312c0bcb0bf8f5a0df6fcb718c68085c4d8a20e
tech-scripts <techscriptsgithub@gmail.com>
2025-04-18 11:02:18 +0500

    Update editor.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

--- misc/tech/editor.sh
+++ misc/tech/editor.sh
@@ -6,17 +6,17 @@ LANG_CONF=$(grep '^lang:' /etc/tech-scripts/choose.conf 2>/dev/null | cut -d' ' 
 if [ "$LANG_CONF" = "Русский" ]; then
     TITLE_EDITOR="Выбор текстового редактора"
     MSG_EDITOR="Выберите текстовый редактор:"
-    TITLE_CUSTOM="Пользовательский редактор"
-    MSG_CUSTOM="Введите команду вашего текстового редактора:"
-    MSG_INVALID="Неверный выбор!"
-    MSG_SUCCESS="Текстовый редактор установлен:"
+    TITLE_CUSTOM_EDITOR="Пользовательский редактор"
+    MSG_CUSTOM_EDITOR="Введите команду вашего текстового редактора:"
+    MSG_INVALID_EDITOR="Неверный выбор!"
+    MSG_SUCCESS_EDITOR="Текстовый редактор установлен:"
 else
     TITLE_EDITOR="Text Editor Selection"
     MSG_EDITOR="Choose your text editor:"
-    TITLE_CUSTOM="Custom Editor"
-    MSG_CUSTOM="Enter the command custom text editor:"
-    MSG_INVALID="Invalid choice!"
-    MSG_SUCCESS="Text editor set to:"
+    TITLE_CUSTOM_EDITOR="Custom Editor"
+    MSG_CUSTOM_EDITOR="Enter the command custom text editor:"
+    MSG_INVALID_EDITOR="Invalid choice!"
+    MSG_SUCCESS_EDITOR="Text editor set to:"
 fi
 
 [ ! -d "/etc/tech-scripts" ] && $SUDO mkdir -p /etc/tech-scripts
@@ -36,14 +36,14 @@ case $EDITOR in
     1) editor="nano" ;;
     2) editor="vim" ;;
     3)
-        editor=$(whiptail --title "$TITLE_CUSTOM" --inputbox "$MSG_CUSTOM" 10 40 3>&1 1>&2 2>&3)
+        editor=$(whiptail --title "$TITLE_CUSTOM_EDITOR" --inputbox "$MSG_CUSTOM_EDITOR" 10 40 3>&1 1>&2 2>&3)
         if [ $? -ne 0 ]; then
             exit 1
         fi
         ;;
     *) 
         echo " "
-        echo "$MSG_INVALID"
+        echo "$MSG_INVALID_EDITOR"
         echo " "
         exit 1
         ;;
@@ -60,5 +60,5 @@ else
 fi
 
 echo " "
-echo "$MSG_SUCCESS $editor"
+echo "$MSG_SUCCESS_EDITOR $editor"
 echo " "
