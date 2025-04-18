16b134e465627b1c63b9ef5df58c43f5c5d687dc
tech-scripts <techscriptsgithub@gmail.com>
2025-04-18 18:12:49 +0500

    Update cpu.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

--- misc/tools/benchmark/cpu.sh
+++ misc/tools/benchmark/cpu.sh
@@ -1,35 +1,36 @@
 #!/bin/bash
 
+install_sysbench() {
+    if command -v apt &>/dev/null; then
+        sudo apt update && sudo apt install -y sysbench
+    elif command -v yum &>/dev/null; then
+        sudo yum install -y sysbench
+    elif command -v dnf &>/dev/null; then
+        sudo dnf install -y sysbench
+    elif command -v zypper &>/dev/null; then
+        sudo zypper install -y sysbench
+    elif command -v pacman &>/dev/null; then
+        sudo pacman -S --noconfirm sysbench
+    elif command -v apk &>/dev/null; then
+        sudo apk add sysbench
+    elif command -v brew &>/dev/null; then
+        brew install sysbench
+    else
+        echo "Не удалось определить пакетный менеджер. Установите sysbench вручную!"
+        exit 1
+    fi
+}
+
 if ! command -v sysbench &>/dev/null; then
-    whiptail --title "Установка необходимых компонентов" --yesno "sysbench не установлен. Хотите установить его?" 10 60
-    if [ $? -eq 0 ]; then
-        if command -v apt &>/dev/null; then
-            sudo apt update && sudo apt install -y sysbench
-        elif command -v yum &>/dev/null; then
-            sudo yum install -y sysbench
-        elif command -v dnf &>/dev/null; then
-            sudo dnf install -y sysbench
-        elif command -v zypper &>/dev/null; then
-            sudo zypper install -y sysbench
-        elif command -v pacman &>/dev/null; then
-            sudo pacman -S --noconfirm sysbench
-        elif command -v apk &>/dev/null; then
-            sudo apk add sysbench
-        elif command -v brew &>/dev/null; then
-            brew install sysbench
-        else
-            echo "Не удалось определить пакетный менеджер. Установите sysbench вручную!"
-            exit 1
-        fi
+    if whiptail --title "Установка необходимых компонентов" --yesno "sysbench не установлен. Хотите установить его?" 10 60; then
+        install_sysbench
     else
         exit 0
     fi
 fi
 
-whiptail --title "Стресс-тест процессора" --yesno "Вы хотите выполнить стресс-тест процессора?" 10 60
-
 show_progress() {
-    ( 
+    (
         for i in {1..100}; do
             sleep 0.1
             echo $i
@@ -37,27 +38,27 @@ show_progress() {
     ) | whiptail --title "Прогресс выполнения" --gauge "" 6 60 0
 }
 
-if [[ $? -eq 0 ]]; then
+if whiptail --title "Стресс-тест процессора" --yesno "Вы хотите выполнить стресс-тест процессора?" 10 60; then
     show_progress &
+    progress_pid=$!
+
     single_core_result=$(sysbench cpu --time=5 --threads=1 run)
     multi_core_result=$(sysbench cpu --time=5 --threads=$(nproc) run)
-    wait
+
+    kill $progress_pid 2>/dev/null
+
     echo ""
     echo "Single core ---"
     echo ""
-    echo -e "CPU speed:"
-    echo -e "$(echo "$single_core_result" | grep "events per second:" | sed -E 's/([0-9]+\.[0-9]+)/\\e[38;2;160;160;160m\1\\e[0m/g')"
+    echo -e "CPU speed: $(echo "$single_core_result" | grep "events per second:" | sed -E 's/([0-9]+\.[0-9]+)/\\e[38;2;160;160;160m\1\\e[0m/g')"
     echo ""
-    echo -e "General statistics:"
-    echo -e "$(echo "$single_core_result" | grep -E "total time:|total number of events:" | sed -E 's/([0-9]+\.[0-9]+|[0-9]+)/\\e[38;2;160;160;160m\1\\e[0m/g')"
+    echo -e "General statistics:\n$(echo "$single_core_result" | grep -E "total time:|total number of events:" | sed -E 's/([0-9]+\.[0-9]+|[0-9]+)/\\e[38;2;160;160;160m\1\\e[0m/g')"
     echo ""
     echo "Multi core ---"
     echo ""
-    echo -e "CPU speed:"
-    echo -e "$(echo "$multi_core_result" | grep "events per second:" | sed -E 's/([0-9]+\.[0-9]+)/\\e[38;2;160;160;160m\1\\e[0m/g')"
+    echo -e "CPU speed: $(echo "$multi_core_result" | grep "events per second:" | sed -E 's/([0-9]+\.[0-9]+)/\\e[38;2;160;160;160m\1\\e[0m/g')"
     echo ""
-    echo -e "General statistics:"
-    echo -e "$(echo "$multi_core_result" | grep -E "total time:|total number of events:" | sed -E 's/([0-9]+\.[0-9]+|[0-9]+)/\\e[38;2;160;160;160m\1\\e[0m/g')"
+    echo -e "General statistics:\n$(echo "$multi_core_result" | grep -E "total time:|total number of events:" | sed -E 's/([0-9]+\.[0-9]+|[0-9]+)/\\e[38;2;160;160;160m\1\\e[0m/g')"
     echo ""
 else
     exit 0
