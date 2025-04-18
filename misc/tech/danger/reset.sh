ef7df25ad0e4bc65d061260f13681c384ddf4527
tech-scripts <techscriptsgithub@gmail.com>
2025-04-18 10:51:36 +0500

    Update reset.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

--- misc/tech/danger/reset.sh
+++ misc/tech/danger/reset.sh
@@ -1,25 +1,10 @@
 #!/bin/bash
 
 SUDO=$(command -v sudo)
-LANG_CONF=$(grep -E '^lang:' /etc/tech-scripts/choose.conf | cut -d' ' -f2)
 
-if [[ $LANG_CONF == "Русский" ]]; then
-    TITLE="Подтверждение удаления"
-    MESSAGE="Вы точно хотите удалить все файлы tech-scripts?"
-    YES="Да"
-    NO="Нет"
-    DELETING="Удаление файлов..."
-    DELETED="Файлы успешно удалены!"
-else
-    TITLE="Delete Confirmation"
-    MESSAGE="Are you sure you want to delete all tech-scripts files?"
-    YES="Yes"
-    NO="No"
-    DELETING="Deleting files..."
-    DELETED="Files deleted successfully!"
-fi
+source /tmp/tech-scripts/misc/localization.sh 
 
-whiptail --title "$TITLE" --yesno "$MESSAGE" --yes-button "$YES" --no-button "$NO" 10 60
+whiptail --title "$TITLE_DANGER" --yesno "$MESSAGE_DANGER" --yes-button "$YES" --no-button "$NO" 10 60
 
 if [ $? -eq 0 ]; then
     echo "$DELETING"
