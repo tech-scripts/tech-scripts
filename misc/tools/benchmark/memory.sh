bb7ba9ff176a11a653e6b2f307b54ab8d2d37d93
tech-scripts <techscriptsgithub@gmail.com>
2025-04-19 22:24:12 +0500

    Update memory.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

--- misc/tools/benchmark/memory.sh
+++ misc/tools/benchmark/memory.sh
@@ -3,24 +3,38 @@
 LANGUAGE=$(grep '^lang:' /etc/tech-scripts/choose.conf | cut -d' ' -f2)
 
 if [ "$LANGUAGE" == "Русский" ]; then
-  TITLE="Стресс-тест памяти"
-  QUESTION="Вы хотите выполнить стресс-тест памяти?"
-  RUNNING="Запуск стресс-теста памяти на 10 секунд..."
-  RESULTS="Результаты теста памяти:"
+  title="стресс-тест памяти"
+  question="вы хотите выполнить стресс-тест памяти?"
+  running="запуск стресс-теста памяти на 10 секунд..."
+  results="результаты теста памяти:"
 else
-  TITLE="Memory Stress Test"
-  QUESTION="Do you want to perform a memory stress test?"
-  RUNNING="Running memory stress test for 10 seconds..."
-  RESULTS="Memory test results:"
+  title="memory stress test"
+  question="do you want to perform a memory stress test?"
+  running="running memory stress test for 10 seconds..."
+  results="memory test results:"
 fi
 
-whiptail --title "$TITLE" --yesno "$QUESTION" 10 60
+whiptail --title "$title" --yesno "$question" 10 60
 if [ $? -eq 0 ]; then
-  echo "$RUNNING"
+  echo "$running"
+  
   sysbench --test=memory --memory-block-size=1K --memory-oper=write --time=10 run > /tmp/sysbench_memory_test.txt
-  echo "$RESULTS"
-  echo "------------------------"
-  grep -E "total time:|total number of events:|min:|avg:|max:|95th percentile:" /tmp/sysbench_memory_test.txt
+  
+  total_time=$(grep "total time:" /tmp/sysbench_memory_test.txt | awk '{print $3}')
+  total_events=$(grep "total number of events:" /tmp/sysbench_memory_test.txt | awk '{print $5}')
+  
+  operations_per_second=$(echo "scale=2; $total_events / $total_time" | bc)
+  
+  total_data_transferred=$(echo "$total_events * 1" | bc)
+  data_transferred_miB=$(echo "scale=2; $total_data_transferred / 1024" | bc)
+  data_transferred_rate=$(echo "scale=2; $data_transferred_miB / $total_time" | bc)
+
+  echo "total operations: $total_events ($operations_per_second per second)"
+  echo "$data_transferred_miB MiB transferred ($data_transferred_rate MiB/sec)"
+  echo "general statistics:"
+  echo "total time: $total_time"
+  echo "total number of events: $total_events"
+
   rm -f /tmp/sysbench_memory_test.txt
 else
   exit 0
