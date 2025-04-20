d0dc51b9f850da26cf73dfd24a7c59502c11a7c7
tech-scripts <techscriptsgithub@gmail.com>
2025-04-21 00:11:22 +0500

    Update disk.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

--- misc/tools/benchmark/disk.sh
+++ misc/tools/benchmark/disk.sh
@@ -3,22 +3,24 @@
 FILE_SIZE="1G"
 LANGUAGE=$(grep '^lang:' /etc/tech-scripts/choose.conf | cut -d' ' -f2)
 
-if [ "$LANGUAGE" == "Русский" ]; then
-    msg_select="Выберите директорию"
-    msg_speed_write="Скорость записи:"
-    msg_speed_read="Скорость чтения:"
-    msg_time_write="Время записи:"
-    msg_time_read="Время чтения:"
-    msg_failed="Не удалось измерить скорость"
-    msg_selected_dir="Выбранная директория:"
+if [ "$LANGUAGE" = "Русский" ]; then
+    MSG_SELECT_DISK="Выберите директорию"
+    MSG_SPEED_WRITE_DISK="Скорость записи:"
+    MSG_SPEED_READ_DISK="Скорость чтения:"
+    MSG_TIME_WRITE_DISK="Время записи:"
+    MSG_TIME_READ_DISK="Время чтения:"
+    MSG_FAILED_DISK="Не удалось измерить скорость"
+    MSG_SELECTED_DIR_DISK="Выбранная директория:"
+    MSG_NO_MOUNTS_DISK="Нет доступных точек монтирования!"
 else
-    msg_select="Select a directory"
-    msg_speed_write="Write speed:"
-    msg_speed_read="Read speed:"
-    msg_time_write="Write time:"
-    msg_time_read="Read time:"
-    msg_failed="Failed to measure speed"
-    msg_selected_dir="Selected directory:"
+    MSG_SELECT_DISK="Select directory"
+    MSG_SPEED_WRITE_DISK="Write speed:"
+    MSG_SPEED_READ_DISK="Read speed:"
+    MSG_TIME_WRITE_DISK="Write time:"
+    MSG_TIME_READ_DISK="Read time:"
+    MSG_FAILED_DISK="Failed to measure speed"
+    MSG_SELECTED_DIR_DISK="Selected directory:"
+    MSG_NO_MOUNTS_DISK="No available mount points!"
 fi
 
 disk_choices=()
@@ -39,7 +41,7 @@ while IFS= read -r line; do
 done < <(lsblk -o NAME,MOUNTPOINT -n -l | grep -v '^\s*$')
 
 if [ ${#disk_choices[@]} -eq 0 ]; then
-    echo "Нет доступных точек монтирования для выбора."
+    echo "$MSG_NO_MOUNTS_DISK"
     exit 1
 fi
 
@@ -48,7 +50,7 @@ for ((i=0; i<${#disk_choices[@]}; i+=2)); do
     menu_items+=("${disk_choices[i]}" "${disk_choices[i+1]}")
 done
 
-selected_disk=$(whiptail --title "$msg_select" --menu "" 15 60 4 "${menu_items[@]}" 3>&1 1>&2 2>&3)
+selected_disk=$(whiptail --title "$MSG_SELECT_DISK" --menu "" 15 60 4 "${menu_items[@]}" 3>&1 1>&2 2>&3)
 
 if [ -z "$selected_disk" ]; then
     exit 0
@@ -66,20 +68,20 @@ temp_file="$selected_mount_point/testfile"
 
 output=$(dd if=/dev/zero of="$temp_file" bs="$FILE_SIZE" count=1 oflag=direct 2>&1)
 write_time=$(echo "$output" | grep -o '[0-9.]* s' | head -n 1)
-write_speed=$(echo "$output" | grep -o '[0-9.]* [MG]B/s' | head -n 1 || echo "$msg_failed")
+write_speed=$(echo "$output" | grep -o '[0-9.]* [MG]B/s' | head -n 1 || echo "$MSG_FAILED_DISK")
 
 output=$(dd if="$temp_file" of=/dev/null bs="$FILE_SIZE" count=1 iflag=direct 2>&1)
 read_time=$(echo "$output" | grep -o '[0-9.]* s' | head -n 1)
-read_speed=$(echo "$output" | grep -o '[0-9.]* [MG]B/s' | head -n 1 || echo "$msg_failed")
+read_speed=$(echo "$output" | grep -o '[0-9.]* [MG]B/s' | head -n 1 || echo "$MSG_FAILED_DISK")
 
 echo ""
-echo -e "$msg_selected_dir \e[38;2;160;160;160m$selected_mount_point\e[0m"
+echo -e "$MSG_SELECTED_DIR_DISK \e[38;2;160;160;160m$selected_mount_point\e[0m"
 echo ""
-echo -e "$msg_speed_write \e[38;2;160;160;160m$write_speed\e[0m"
-echo -e "$msg_time_write \e[38;2;160;160;160m$write_time\e[0m"
+echo -e "$MSG_SPEED_WRITE_DISK \e[38;2;160;160;160m$write_speed\e[0m"
+echo -e "$MSG_TIME_WRITE_DISK \e[38;2;160;160;160m$write_time\e[0m"
 echo ""
-echo -e "$msg_speed_read \e[38;2;160;160;160m$read_speed\e[0m"
-echo -e "$msg_time_read \e[38;2;160;160;160m$read_time\e[0m"
+echo -e "$MSG_SPEED_READ_DISK \e[38;2;160;160;160m$read_speed\e[0m"
+echo -e "$MSG_TIME_READ_DISK \e[38;2;160;160;160m$read_time\e[0m"
 echo ""
 
 rm -f "$temp_file"
