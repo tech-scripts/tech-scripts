6fb2b9f1f9070ce4601863b6b8df64de98a3f70b
tech-scripts <techscriptsgithub@gmail.com>
2025-04-22 02:07:16 +0500

    Update vm.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

--- misc/proxmox/vm.sh
+++ misc/proxmox/vm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-source /tmp/tech-scripts/misc/localization.sh
-source /tmp/tech-scripts/misc/variables.sh
+source /etc/tech-scripts/localization.sh
+source /etc/tech-scripts/variables.sh
 
 if ! command -v qm &> /dev/null; then
     echo ""
