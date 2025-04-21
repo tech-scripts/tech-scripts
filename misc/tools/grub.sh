02bba61c95ac075042c1128c61173002b13a17cc
tech-scripts <techscriptsgithub@gmail.com>
2025-04-21 10:51:34 +0500

    Update grub.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

--- misc/tools/grub.sh
+++ misc/tools/grub.sh
@@ -1,36 +1,24 @@
 #!/bin/bash
 
-SUDO=$(command -v sudo)
-LANGUAGE=$(grep '^lang:' /etc/tech-scripts/choose.conf | cut -d' ' -f2)
-
-if [ "$LANGUAGE" = "Русский" ]; then
-    INPUT_MSG="Введите количество секунд для задержки перед запуском:"
-    CONFIRM_PROMPT="Вы ввели: %s секунд. Это правильно?"
-    ERROR_MSG="Ошибка: Введите целое число."
-    SUCCESS_MSG="Задержка перед запуском установлена на %s секунд. Конфигурация GRUB обновлена!"
-else
-    INPUT_MSG="Enter the number of seconds for the boot delay:"
-    CONFIRM_PROMPT="You entered: %s seconds. Is this correct?"
-    ERROR_MSG="Error: Please enter a whole number."
-    SUCCESS_MSG="Boot delay set to %s seconds. GRUB configuration updated!"
-fi
+source /tmp/tech-scripts/misc/localization.sh
+source /tmp/tech-scripts/misc/variables.sh
 
 is_number() {
     [[ $1 =~ ^[0-9]+$ ]]
 }
 
 while true; do
-    delay=$(whiptail --inputbox "$INPUT_MSG" 0 0 3>&1 1>&2 2>&3)
+    delay=$(whiptail --inputbox "$ERROR_MSG_NUMBER_GRUB" 0 0 3>&1 1>&2 2>&3)
     [ $? -ne 0 ] && exit 1
     if is_number "$delay"; then
-        if whiptail --yesno "$(printf "$CONFIRM_PROMPT" "$delay")" 7 40; then
+        if whiptail --yesno "$(printf "$CONFIRM_PROMPT_GRUB" "$delay")" 7 40; then
             break
         fi
     else
-        whiptail --msgbox "$ERROR_MSG" 7 40
+        whiptail --msgbox "$ERROR_MSG_GRUB" 7 40
     fi
 done
 
 $SUDO sed -i "s/GRUB_TIMEOUT=.*/GRUB_TIMEOUT=$delay/" /etc/default/grub
 $SUDO update-grub
-echo "$(printf "$SUCCESS_MSG" "$delay")"
+echo "$(printf "$SUCCESS_MSG_GRUB" "$delay")"
