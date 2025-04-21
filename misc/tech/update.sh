51b2af759c2164f03492a33dbb910976e747b61f
tech-scripts <techscriptsgithub@gmail.com>
2025-04-21 22:29:03 +0500

    Update update.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

--- misc/tech/update.sh
+++ misc/tech/update.sh
@@ -8,13 +8,13 @@ LANGUAGE=$(grep '^lang:' /etc/tech-scripts/choose.conf | cut -d' ' -f2)
 
 cd /tmp
 $SUDO rm -rf /tmp/tech-scripts
-git clone --depth 1 https://github.com/tech-scripts/linux.git /tmp/tech-scripts
+git clone --depth 1 https://github.com/tech-scripts/tech-scripts.git /tmp/tech-scripts
 $SUDO rm -rf /usr/local/bin/tech
 cd "/tmp/tech-scripts/misc/tech"
 chmod +x tech.sh
 ./tech.sh
 echo ""
-if [ "LANGUAGE" ]; then
+if [ "$LANGUAGE" ]; then
 echo "Обновление завершено!"
 else
 echo "Update completed!"
