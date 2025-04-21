ac2ac0acf61852ed50dc9867be3e503b17c48aa6
tech-scripts <techscriptsgithub@gmail.com>
2025-04-22 02:09:02 +0500

    Update autoupdate.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

--- misc/tools/autoupdate.sh
+++ misc/tools/autoupdate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-source /tmp/tech-scripts/misc/localization.sh
-source /tmp/tech-scripts/misc/variables.sh
+source /etc/tech-scripts/localization.sh
+source /etc/tech-scripts/variables.sh
 
 if (whiptail --title "$TITLE_AUTOUPDATE" --yesno "$QUESTION_AUTOUPDATE" 10 60); then
     echo " "
