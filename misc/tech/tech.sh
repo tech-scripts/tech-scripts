e041658a994641467401d81954dcaf9933de30f1
tech-scripts <techscriptsgithub@gmail.com>
2025-04-22 01:19:23 +0500

    Update tech.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

--- misc/tech/tech.sh
+++ misc/tech/tech.sh
@@ -3,42 +3,15 @@
 source /tmp/tech-scripts/misc/localization.sh
 source /tmp/tech-scripts/misc/variables.sh
 
-LANGUAGE=$(grep '^lang:' /etc/tech-scripts/choose.conf | cut -d' ' -f2)
-
-if [ "$LANGUAGE" = "Русский" ]; then
-    TITLE_ADD_TECH="Быстрый доступ"
-    MSG_ADD_TECH="Хотите добавить команду tech для быстрого доступа?"
-    TITLE_REMOVE_TECH="Удаление команды"
-    MSG_REMOVE_TECH="Команда tech уже существует. Хотите удалить её?"
-    MSG_REMOVED_TECH="Команда tech успешно удалена!"
-    TITLE_UPDATE_TECH="Обновление команды"
-    MSG_UPDATE_TECH="Команда tech уже существует. Хотите обновить её?"
-    MSG_UPDATED_TECH="Команда tech успешно обновлена!"
-    UNKNOWN_COMMAND_TECH="Неизвестная команда:"
-    USAGE_TECH="Использование: tech [update|config|ssh|...]"
-else
-    TITLE_ADD_TECH="Quick access"
-    MSG_ADD_TECH="Do you want to add the tech command for quick access?"
-    TITLE_REMOVE_TECH="Remove command"
-    MSG_REMOVE_TECH="The tech command already exists. Do you want to remove it?"
-    MSG_REMOVED_TECH="The tech command has been successfully removed!"
-    TITLE_UPDATE_TECH="Update command"
-    MSG_UPDATE_TECH="The tech command already exists. Do you want to update it?"
-    MSG_UPDATED_TECH="The tech command has been successfully updated!"
-    UNKNOWN_COMMAND_TECH="Unknown command:"
-    USAGE_TECH="Usage: tech [update|config|ssh|...]"
-fi
-
 TECH_SCRIPT=$(cat <<EOF
 #!/bin/bash
 
-REPO_URL_TECH="https://github.com/tech-scripts/linux.git"
+REPO_URL_TECH="https://github.com/tech-scripts/tech-scripts.git"
 CLONE_DIR_TECH="/tmp/tech-scripts"
 
 source /tmp/tech-scripts/misc/localization.sh
 source /tmp/tech-scripts/misc/variables.sh
-UNKNOWN_COMMAND_TECH="Неизвестная команда:"
-USAGE_TECH="Использование: tech [update|config|ssh|...]"
+
 run_script() {
     local script_dir="\$1"
     local script_name="\$2"
