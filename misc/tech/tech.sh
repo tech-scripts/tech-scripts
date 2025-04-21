d3a40bb5df75fec4bb86e4479a8836c848ccb57f
tech-scripts <techscriptsgithub@gmail.com>
2025-04-22 01:08:27 +0500

    Update tech.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

--- misc/tech/tech.sh
+++ misc/tech/tech.sh
@@ -3,45 +3,47 @@
 source /tmp/tech-scripts/misc/localization.sh
 source /tmp/tech-scripts/misc/variables.sh
 
-if [ "$LANG_CONF" == "Русский" ]; then
-    title_add="Быстрый доступ"
-    msg_add="Хотите добавить команду tech для быстрого доступа?"
-    title_remove="Удаление команды"
-    msg_remove="Команда tech уже существует. Хотите удалить её?"
-    msg_removed="Команда tech успешно удалена!"
-    title_update="Обновление команды"
-    msg_update="Команда tech уже существует. Хотите обновить её?"
-    msg_updated="Команда tech успешно обновлена!"
-    msg_add_complete="Команда tech успешно добавлена!"
-    unknown_command="Неизвестная команда: \$1"
-    usage="Использование: tech [lxc|vm|ssh alert|...]"
+LANGUAGE=$(grep '^lang:' /etc/tech-scripts/choose.conf | cut -d' ' -f2)
+
+if [ "$LANGUAGE" = "Русский" ]; then
+    TITLE_ADD_TECH="Быстрый доступ"
+    MSG_ADD_TECH="Хотите добавить команду tech для быстрого доступа?"
+    TITLE_REMOVE_TECH="Удаление команды"
+    MSG_REMOVE_TECH="Команда tech уже существует. Хотите удалить её?"
+    MSG_REMOVED_TECH="Команда tech успешно удалена!"
+    TITLE_UPDATE_TECH="Обновление команды"
+    MSG_UPDATE_TECH="Команда tech уже существует. Хотите обновить её?"
+    MSG_UPDATED_TECH="Команда tech успешно обновлена!"
+    MSG_ADD_COMPLETE_TECH="Команда tech успешно добавлена!"
+    UNKNOWN_COMMAND_TECH="Неизвестная команда: \$1"
+    USAGE_TECH="Использование: tech [lxc|vm|ssh alert|...]"
 else
-    title_add="Quick access"
-    msg_add="Do you want to add the tech command for quick access?"
-    title_remove="Remove command"
-    msg_remove="The tech command already exists. Do you want to remove it?"
-    msg_removed="The tech command has been successfully removed!"
-    title_update="Update command"
-    msg_update="The tech command already exists. Do you want to update it?"
-    msg_updated="The tech command has been successfully updated!"
-    msg_add_complete="The tech command has been successfully added!"
-    unknown_command="Unknown command: \$1"
-    usage="Usage: tech [lxc|vm|ssh alert|...]"
+    TITLE_ADD_TECH="Quick access"
+    MSG_ADD_TECH="Do you want to add the tech command for quick access?"
+    TITLE_REMOVE_TECH="Remove command"
+    MSG_REMOVE_TECH="The tech command already exists. Do you want to remove it?"
+    MSG_REMOVED_TECH="The tech command has been successfully removed!"
+    TITLE_UPDATE_TECH="Update command"
+    MSG_UPDATE_TECH="The tech command already exists. Do you want to update it?"
+    MSG_UPDATED_TECH="The tech command has been successfully updated!"
+    MSG_ADD_COMPLETE_TECH="The tech command has been successfully added!"
+    UNKNOWN_COMMAND_TECH="Unknown command: \$1"
+    USAGE_TECH="Usage: tech [lxc|vm|ssh alert|...]"
 fi
 
 TECH_SCRIPT=$(cat <<EOF
 #!/bin/bash
 
-REPO_URL="https://github.com/tech-scripts/tech-scripts.git"
-CLONE_DIR="/tmp/tech-scripts"
+REPO_URL_TECH="https://github.com/tech-scripts/linux.git"
+CLONE_DIR_TECH="/tmp/tech-scripts"
 
-unknown_command="$unknown_command"
-usage="$usage"
+unknown_command="\$UNKNOWN_COMMAND_TECH"
+usage="\$USAGE_TECH"
 
 run_script() {
     local script_dir="\$1"
     local script_name="\$2"
-    cd "\$CLONE_DIR/misc/\$script_dir"
+    cd "\$CLONE_DIR_TECH/misc/\$script_dir"
     chmod +x "\$script_name"
     ./"\$script_name"
 }
@@ -56,71 +58,39 @@ fi
 combined_args="\$*"
 
 case "\$combined_args" in
-    "update")
-        run_script "tech" "update.sh"
-        ;;
-    "lxc")
-        run_script "proxmox" "lxc.sh"
-        ;;
-    "vm")
-        run_script "proxmox" "vm.sh"
-        ;;
-    "disk")
-        run_script "tools/benchmark" "disk.sh"
-        ;;
-    "cpu")
-        run_script "tools/benchmark" "cpu.sh"
-        ;;
-    "memory")
-        run_script "tools/benchmark" "memory.sh"
-        ;;
-    "autoupdate")
-        run_script "tools" "autoupdate.sh"
-        ;;
-    "grub")
-        run_script "tools" "grub.sh"
-        ;;
-    "ssh")
-        run_script "tools" "ssh.sh"
-        ;;
-    "startup")
-        run_script "tools" "startup.sh"
-        ;;
-    "swap")
-        run_script "tools" "swap.sh"
-        ;;
-    "system")
-        run_script "tools" "system.sh"
-        ;;
-    "config")
-         run_script "tools" "config.sh"
-         ;;
-    *)
-        echo " "
-        echo "\$unknown_command"
-        echo "\$usage"
-        echo " "
-        exit 1
-        ;;
+    "update") run_script "tech" "update.sh" ;;
+    "lxc") run_script "proxmox" "lxc.sh" ;;
+    "vm") run_script "proxmox" "vm.sh" ;;
+    "disk") run_script "tools/benchmark" "disk.sh" ;;
+    "cpu") run_script "tools/benchmark" "cpu.sh" ;;
+    "memory") run_script "tools/benchmark" "memory.sh" ;;
+    "autoupdate") run_script "tools" "autoupdate.sh" ;;
+    "grub") run_script "tools" "grub.sh" ;;
+    "ssh") run_script "tools" "ssh.sh" ;;
+    "startup") run_script "tools" "startup.sh" ;;
+    "swap") run_script "tools" "swap.sh" ;;
+    "system") run_script "tools" "system.sh" ;;
+    "config") run_script "tools" "config.sh" ;;
+    *) echo " "; echo "\$unknown_command"; echo "\$usage"; echo " "; exit 1 ;;
 esac
 EOF
 )
 
 if [ -f /usr/local/bin/tech ]; then
-    whiptail --title "$title_update" --yesno "$msg_update" 10 40
+    whiptail --title "$TITLE_UPDATE_TECH" --yesno "$MSG_UPDATE_TECH" 10 40
     if [ $? -eq 0 ]; then
         $SUDO rm /usr/local/bin/tech
         $SUDO tee /usr/local/bin/tech > /dev/null <<< "$TECH_SCRIPT"
         $SUDO chmod +x /usr/local/bin/tech
         echo " "
-        echo "$msg_updated"
+        echo "$MSG_UPDATED_TECH"
         echo " "
     else
-        whiptail --title "$title_remove" --yesno "$msg_remove" 10 40
+        whiptail --title "$TITLE_REMOVE_TECH" --yesno "$MSG_REMOVE_TECH" 10 40
         if [ $? -eq 0 ]; then
             $SUDO rm /usr/local/bin/tech
             echo " "
-            echo "$msg_removed"
+            echo "$MSG_REMOVED_TECH"
             echo " "
         else
             exit 0
@@ -129,4 +99,8 @@ if [ -f /usr/local/bin/tech ]; then
 else
     $SUDO tee /usr/local/bin/tech > /dev/null <<< "$TECH_SCRIPT"
     $SUDO chmod +x /usr/local/bin/tech
+    echo " "
+    echo "$MSG_ADD_COMPLETE_TECH"
+    echo " "
 fi
+   
