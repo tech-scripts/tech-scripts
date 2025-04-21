fdaaccfacb3358788333a60c75733dea6df8597a
tech-scripts <techscriptsgithub@gmail.com>
2025-04-21 14:53:37 +0500

    Update variables.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

--- misc/variables.sh
+++ misc/variables.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 SUDO=$(command -v sudo)
+LANGUAGE=$(grep '^lang:' /etc/tech-scripts/choose.conf | cut -d' ' -f2)
 EDITOR=$(grep '^editor:' /etc/tech-scripts/choose.conf | cut -d ' ' -f 2)
 CONFIG_FILE="/etc/tech-scripts/choose.conf"
 SCRIPT_DIR_SSH="/usr/local/tech-scripts"
