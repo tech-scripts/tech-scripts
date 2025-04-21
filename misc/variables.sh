53ad35c2d59b21c32f065cdb3db8d42ecf4ef256
tech-scripts <techscriptsgithub@gmail.com>
2025-04-21 14:58:58 +0500

    Update variables.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

--- misc/variables.sh
+++ misc/variables.sh
@@ -6,3 +6,6 @@ EDITOR=$(grep '^editor:' /etc/tech-scripts/choose.conf | cut -d ' ' -f 2)
 CONFIG_FILE="/etc/tech-scripts/choose.conf"
 SCRIPT_DIR_SSH="/usr/local/tech-scripts"
 CONFIG_FILE_SSH="/etc/tech-scripts/alert.conf"
+AUTOSTART_SCRIPT="/usr/local/tech-scripts/autostart.sh"
+SERVICE_FILE="/etc/systemd/system/autostart.service"
+SERVICE_NAME="autostart.service"
