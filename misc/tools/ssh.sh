6d26cded476c490cf25713a49649756df8f92801
tech-scripts <techscriptsgithub@gmail.com>
2025-04-22 01:46:04 +0500

    Update ssh.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

--- misc/tools/ssh.sh
+++ misc/tools/ssh.sh
@@ -197,13 +197,13 @@ if yes_no_box "$CREATE_NOTIFY_SSH" "$CREATE_ALERT_SSH"; then
             TELEGRAM_THREAD_ID=$(input_box "Telegram Thread ID" "$SUPER_GROUP_ID_SSH")
             
             if send_test_message "$TELEGRAM_BOT_TOKEN" "$TELEGRAM_CHAT_ID" "$TELEGRAM_THREAD_ID" "$TEST_MESSAGE_SSH"; then
-                if yes_no_box "$PROHIBIT_SOUND_SSH" ""; then
+                if yes_no_box " " "$PROHIBIT_SOUND_SSH"; then
                     SEND_SILENT=true
                 else
                     SEND_SILENT=false
                 fi
                 
-                if yes_no_box "$PROHIBIT_FORWARDING_SSH" ""; then
+                if yes_no_box " " "$PROHIBIT_FORWARDING_SSH"; then
                     PROTECT_CONTENT=true
                 else
                     PROTECT_CONTENT=false
