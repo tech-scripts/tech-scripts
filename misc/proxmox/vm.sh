2970778f114c8f66f306a5b44936661f62ff5c8d
tech-scripts <techscriptsgithub@gmail.com>
2025-04-23 18:53:13 +0500

    Update vm.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

--- misc/proxmox/vm.sh
+++ misc/proxmox/vm.sh
@@ -86,7 +86,6 @@ while true; do
             9) break ;;
             *) show_message "$MSG_ERROR" ;;
         esac
-
         if ! whiptail --title "$SELECT_ACTION" --yesno "$CONTINUE_VM" 7 50; then
             break
         fi
