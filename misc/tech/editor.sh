3ef51e1f59378b4020d2756d6b47b4845a4fc979
tech-scripts <techscriptsgithub@gmail.com>
2025-04-27 16:57:08 +0500

    Update editor.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

--- misc/tech/editor.sh
+++ misc/tech/editor.sh
@@ -6,7 +6,7 @@ source /etc/tech-scripts/variables.sh
 [ ! -d "/etc/tech-scripts" ] && $SUDO mkdir -p /etc/tech-scripts
 [ ! -f "/etc/tech-scripts/choose.conf" ] && $SUDO touch /etc/tech-scripts/choose.conf
 
-EDITOR=$(whiptail --title "$TITLE_EDITOR" --menu "$MSG_EDITOR" 12 40 3 \
+EDITOR=$(whiptail --title "$TITLE_EDITOR" --menu "$MSG_EDITOR" --yes-button "$YES" --no-button "$NO" 12 40 3 \
     1 "nano" \
     2 "vim" \
     3 "Custom" \
@@ -20,7 +20,7 @@ case $EDITOR in
     1) editor="nano" ;;
     2) editor="vim" ;;
     3)
-        editor=$(whiptail --title "$TITLE_CUSTOM_EDITOR" --inputbox "$MSG_CUSTOM_EDITOR" 10 40 3>&1 1>&2 2>&3)
+        editor=$(whiptail --title "$TITLE_CUSTOM_EDITOR" --inputbox "$MSG_CUSTOM_EDITOR" --yes-button "$YES" --no-button "$NO" 10 40 3>&1 1>&2 2>&3)
         if [ $? -ne 0 ]; then
             exit 1
         fi
