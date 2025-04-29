27bbad231ae532a77ca91f2c193107871f2a1013
tech-scripts <techscriptsgithub@gmail.com>
2025-04-29 12:37:07 +0500

    Update need.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

--- misc/tools/need.sh
+++ misc/tools/need.sh
@@ -30,35 +30,36 @@ check_module() {
     fi
 }
 
-echo "Проверка модулей ядра:"
+echo -e "\nКатегория: Системные модули"
 check_module "overlay" "overlay" "/sys/module/overlay"
 check_module "br_netfilter" "br_netfilter" "/sys/module/br_netfilter"
 check_module "ip_tables" "ip_tables" "/sys/module/ip_tables"
 check_module "ip6_tables" "ip6_tables" "/sys/module/ip6_tables"
 check_module "nf_nat" "nf_nat" "/sys/module/nf_nat"
+
+echo -e "\nКатегория: Управление ресурсами"
 check_module "cgroup" "cgroup" "/sys/module/cgroup"
+check_module "cgroups" "cgroups" "/proc/cgroups"
+
+echo -e "\nКатегория: Файловые системы"
 check_module "fuse" "FUSE" "/sys/fs/fuse"
-check_module "keyring" "Keyctl" "/proc/keys"
 check_module "nfs" "NFS" "/proc/fs/nfs"
 check_module "cifs" "SMB/KIFC" "/proc/fs/smb"
+check_module "overlayfs" "OverlayFS" "/sys/fs/overlayfs"
 
-echo -e "\nПроверка параметров ядра:"
-check_module "cgroups" "cgroups" "/proc/cgroups"
-check_module "mount" "mount namespaces" "/proc/self/mountinfo"
-check_module "capabilities" "capabilities" "/proc/sys/kernel/cap_last"
-check_module "hostname" "UTS namespace" "/proc/sys/kernel/hostname"
+echo -e "\nКатегория: Сетевые модули"
 check_module "ip_forward" "Network namespaces" "/proc/sys/net/ipv4/ip_forward"
-check_module "keys" "Keyrings" "/proc/sys/kernel/keys"
+check_module "capabilities" "capabilities" "/proc/sys/kernel/cap_last"
+
+echo -e "\nКатегория: IPC (Межпроцессное взаимодействие)"
 check_module "shmmax" "Shared Memory" "/proc/sys/kernel/shmmax"
 check_module "msgmax" "Message Queues" "/proc/sys/kernel/msgmax"
 check_module "sem" "Semaphores" "/proc/sys/kernel/sem"
 
-echo -e "\nПроверка файловых систем:"
-check_module "cgroup" "cgroups v2" "/sys/fs/cgroup"
-check_module "overlayfs" "OverlayFS" "/sys/fs/overlayfs"
-
-echo -e "\nПроверка настроек безопасности:"
+echo -e "\nКатегория: Безопасность"
 check_module "audit" "Kernel Auditing" "/proc/sys/kernel/audit"
 check_module "lockdown" "Kernel Lockdown" "/proc/sys/kernel/lockdown"
 
-echo -e "\nПроверка завершена."
+echo -e "\nКатегория: Уникальные идентификаторы"
+check_module "hostname" "UTS namespace" "/proc/sys/kernel/hostname"
+check_module "keys" "Keyrings" "/proc/sys/kernel/keys"
