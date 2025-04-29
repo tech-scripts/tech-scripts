125666880ee27ccc18ec0fdd0d68c02a153e211f
tech-scripts <techscriptsgithub@gmail.com>
2025-04-29 13:29:38 +0500

    Update need.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

--- misc/tools/need.sh
+++ misc/tools/need.sh
@@ -30,33 +30,75 @@ check_module() {
     fi
 }
 
-echo -e "\nСистемные модули"
-check_module "overlay" "overlay" "/sys/module/overlay"
-check_module "br_netfilter" "br_netfilter" "/sys/module/br_netfilter"
-check_module "ip_tables" "ip_tables" "/sys/module/ip_tables"
-check_module "ip6_tables" "ip6_tables" "/sys/module/ip6_tables"
-check_module "nf_nat" "nf_nat" "/sys/module/nf_nat"
+choice=$(whiptail --title "Выбор проверки модулей" --menu "Выберите, какие модули проверить:" 15 60 4 \
+"1" "Docker" \
+"2" "Podman" \
+"3" "LXC" \
+"4" "Все" 3>&1 1>&2 2>&3)
 
-echo -e "\nФайловые системы"
-check_module "fuse" "FUSE" "/sys/fs/fuse"
-check_module "nfs" "NFS" "/proc/fs/nfsd"
-check_module "cifs" "SMB/KIFC" "/proc/fs/cifs"
-check_module "overlay" "OverlayFS" "/sys/module/overlay"
+case $choice in
+    1)
+        modules=("overlay" "br_netfilter" "ip_tables" "ip6_tables" "nf_nat")
+        echo -e "\nПроверка модулей для Docker:"
+        ;;
+    2)
+        modules=("overlay" "br_netfilter" "ip_tables" "ip6_tables" "nf_nat" "cgroup" "namespace")
+        echo -e "\nПроверка модулей для Podman:"
+        ;;
+    3)
+        modules=("overlay" "br_netfilter" "ip_tables" "ip6_tables" "nf_nat" "cgroup" "namespace")
+        echo -e "\nПроверка модулей для LXC:"
+        ;;
+    4)
+        modules=("overlay" "br_netfilter" "ip_tables" "ip6_tables" "nf_nat" "cgroup" "namespace" "fuse" "nfs" "cifs" "seccomp" "audit" "lockdown")
+        echo -e "\nПроверка всех модулей:"
+        ;;
+    *)
+        echo "Неверный выбор."
+        exit 1
+        ;;
+esac
 
-echo -e "\nСетевые модули"
-check_module "ip_forward" "Network namespaces" "/proc/sys/net/ipv4/ip_forward"
-check_module "capabilities" "capabilities" "/proc/sys/kernel/cap_last_cap"
-
-echo -e "\nIPC (Межпроцессное взаимодействие)"
-check_module "shmmax" "Shared Memory" "/proc/sys/kernel/shmmax"
-check_module "msgmax" "Message Queues" "/proc/sys/kernel/msgmax"
-check_module "sem" "Semaphores" "/proc/sys/kernel/sem"
-
-echo -e "\nБезопасность"
-check_module "audit" "Kernel Auditing" "/proc/sys/kernel/audit"
-check_module "lockdown" "Kernel Lockdown" "/proc/sys/kernel/lockdown"
-check_module "cgroups" "cgroups" "/proc/cgroups"
-
-echo -e "\nУникальные идентификаторы"
-check_module "hostname" "UTS namespace" "/proc/sys/kernel/hostname"
-check_module "keys" "Keyrings" "/proc/sys/kernel/keys"
+for module in "${modules[@]}"; do
+    case $module in
+        "overlay")
+            check_module "overlay" "OverlayFS" "/sys/module/overlay"
+            ;;
+        "br_netfilter")
+            check_module "br_netfilter" "br_netfilter" "/sys/module/br_netfilter"
+            ;;
+        "ip_tables")
+            check_module "ip_tables" "ip_tables" "/sys/module/ip_tables"
+            ;;
+        "ip6_tables")
+            check_module "ip6_tables" "ip6_tables" "/sys/module/ip6_tables"
+            ;;
+        "nf_nat")
+            check_module "nf_nat" "nf_nat" "/sys/module/nf_nat"
+            ;;
+        "cgroup")
+            check_module "cgroup" "cgroups" "/proc/cgroups"
+            ;;
+        "namespace")
+            check_module "namespace" "Namespaces" "/proc/self/ns"
+            ;;
+        "fuse")
+            check_module "fuse" "FUSE" "/sys/fs/fuse"
+            ;;
+        "nfs")
+            check_module "nfs" "NFS" "/proc/fs/nfsd"
+            ;;
+        "cifs")
+            check_module "cifs" "SMB/CIFS" "/proc/fs/cifs"
+            ;;
+        "seccomp")
+            check_module "seccomp" "Seccomp" "/proc/sys/kernel/seccomp"
+            ;;
+        "audit")
+            check_module "audit" "Kernel Auditing" "/proc/sys/kernel/audit"
+            ;;
+        "lockdown")
+            check_module "lockdown" "Kernel Lockdown" "/proc/sys/kernel/lockdown"
+            ;;
+    esac
+done
