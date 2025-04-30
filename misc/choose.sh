8f776db85f0acf2f2799f25ac26a1f16639b20a0
tech-scripts <techscriptsgithub@gmail.com>
2025-04-30 15:32:49 +0500

    Update choose.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

--- misc/choose.sh
+++ misc/choose.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-SCRIPT_DIR="/tmp/tech-scripts/misc/tech"
-cd "$SCRIPT_DIR" || exit 1
-
-[ -f "lang.sh" ] && chmod +x "lang.sh" && ./lang.sh
-
-shopt -s nullglob
-for SCRIPT in *.sh; do
-    [[ "$SCRIPT" == "lang.sh" || "$SCRIPT" == "update.sh" ]] && continue
-    chmod +x "$SCRIPT" && ./"$SCRIPT"
+for script in /tmp/tech-scripts/misc/tech/{tech,lang,access,editor}.sh; do
+    chmod +x "$script" && "$script"
 done
