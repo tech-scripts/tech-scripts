2e8dda00cd0393975fcf5a4d81bd91cd442d27a0
tech-scripts <techscriptsgithub@gmail.com>
2025-04-30 13:37:04 +0500

    Create access.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

---
+++ misc/tech/access.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+CONFIG_FILE="/etc/tech-scripts/choose.conf"
+
+ACCESS_LEVEL=$(whiptail --title "Выбор уровня доступа" --menu "Выберите уровень доступа к скриптам:" 15 60 4 \
+"1" "По умолчанию (755)" \
+"2" "Только владелец (700)" \
+"3" "Владелец и группа (770)" \
+"4" "Все (777)" 3>&1 1>&2 2>&3)
+
+if [ $? != 0 ]; then
+    exit 1
+fi
+
+case $ACCESS_LEVEL in
+    1)
+        ACCESS_VALUE=755
+        ACCESS_TEXT="По умолчанию (755)"
+        ;;
+    2)
+        ACCESS_VALUE=700
+        ACCESS_TEXT="Только владелец (700)"
+        ;;
+    3)
+        ACCESS_VALUE=770
+        ACCESS_TEXT="Владелец и группа (770)"
+        ;;
+    4)
+        ACCESS_VALUE=777
+        ACCESS_TEXT="Все (777)"
+        ;;
+    *)
+        exit 1
+        ;;
+esac
+
+awk -v access_value="access: $ACCESS_VALUE" 'NR==2 {$0=access_value} 1' "$CONFIG_FILE" > "$CONFIG_FILE.tmp" && mv "$CONFIG_FILE.tmp" "$CONFIG_FILE"
+
+echo ""
+echo "Уровень доступа установлен: $ACCESS_TEXT"
+echo ""
