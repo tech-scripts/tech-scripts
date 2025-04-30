c2726c24ee616945ee44b6ea046896ec9094a781
tech-scripts <techscriptsgithub@gmail.com>
2025-04-30 17:10:48 +0500

    Update start.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

--- misc/start.sh
+++ misc/start.sh
@@ -65,7 +65,7 @@ EXCLUDE_FILES=("start.sh" "choose.sh" "localization.sh" "variables.sh" "*.tmp")
 source /etc/tech-scripts/localization.sh
 source /etc/tech-scripts/variables.sh
 
-IFS=' ' read -r -a directories <<< "$BASIC_DIRECTORY"
+[ -n "$BASIC_DIRECTORY" ] && IFS=' ' read -r -a directories <<< "$BASIC_DIRECTORY"
 
 for dir in "${directories[@]}"; do
     [ -d "$dir" ] && [ "$(stat -c "%a" "$dir")" != "$ACCESS" ] && $SUDO chmod "$ACCESS" "$dir"
