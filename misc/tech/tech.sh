9b95360c7e84dd986ac62e3893e8c01fb6b945bc
tech-scripts <techscriptsgithub@gmail.com>
2025-04-30 18:07:47 +0500

    Update tech.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

--- misc/tech/tech.sh
+++ misc/tech/tech.sh
@@ -19,11 +19,10 @@ source /etc/tech-scripts/variables.sh
 BASIC_DIRECTORY=$(echo "$BASIC_DIRECTORY" | tr -s ' ')
 
 if [ -n "$BASIC_DIRECTORY" ]; then
-    IFS=' ' read -r -a directories <<< "$BASIC_DIRECTORY"
+    directories=($(echo "$BASIC_DIRECTORY" | grep -v '^$'))  # Фильтрация пустых строк
 
     for dir in "${directories[@]}"; do
-        echo "Processing directory: '$dir'"
-        if [ -n "$dir" ] && [ -d "$dir" ]; then
+        if [ -d "$dir" ]; then
             if [ "$(stat -c "%a" "$dir")" != "$ACCESS" ]; then
                 $SUDO chmod "$ACCESS" "$dir"
             fi
