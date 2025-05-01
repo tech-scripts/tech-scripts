c012b19509ae95356f893080b30ac7f574f459cc
tech-scripts <techscriptsgithub@gmail.com>
2025-05-01 19:23:12 +0500

    Update start.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

--- misc/start.sh
+++ misc/start.sh
@@ -70,7 +70,7 @@ BASIC_DIRECTORY=$(echo "$BASIC_DIRECTORY" | tr -s ' ')
 [ -n "$BASIC_DIRECTORY" ] && IFS=' ' read -r -a directories <<< "$BASIC_DIRECTORY"
 
 for dir in "${directories[@]}"; do
-    [ -d "$dir" ] && [ "$(stat -c "%a" "$dir")" != "$ACCESS" ] && $SUDO chmod "$ACCESS" "$dir"
+    [ -d "$dir" ] && [ "$(stat -c "%a" "$dir")" != "$ACCESS" ] && $SUDO chmod -R "$ACCESS" "$dir"
 done
 
 get_relative_path() {
