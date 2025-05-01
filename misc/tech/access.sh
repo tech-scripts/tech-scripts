b8587b4da32fbe0e92ad847b5cd94417b100cecf
tech-scripts <techscriptsgithub@gmail.com>
2025-05-01 19:38:23 +0500

    Update access.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

--- misc/tech/access.sh
+++ misc/tech/access.sh
@@ -21,6 +21,14 @@ esac
 
 sed -i "2s/.*/access: $ACCESS_VALUE/" "$CONFIG_FILE"
 
+BASIC_DIRECTORY=$(echo "$BASIC_DIRECTORY" | tr -s ' ')
+
+[ -n "$BASIC_DIRECTORY" ] && IFS=' ' read -r -a directories <<< "$BASIC_DIRECTORY"
+
+for dir in "${directories[@]}"; do
+    [ -d "$dir" ] && [ "$(stat -c "%a" "$dir")" != "$ACCESS" ] && $SUDO chmod -R "$ACCESS" "$dir"
+done
+
 echo ""
 echo "$ACCESS_SET_TEXT $ACCESS_VALUE"
 echo ""
