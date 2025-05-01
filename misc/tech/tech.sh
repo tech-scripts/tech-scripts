0152f49cd216d18980cb6aeee01c6e28f0f4e2df
tech-scripts <techscriptsgithub@gmail.com>
2025-05-01 18:34:58 +0500

    Update tech.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

--- misc/tech/tech.sh
+++ misc/tech/tech.sh
@@ -18,7 +18,7 @@ source /etc/tech-scripts/variables.sh
 
 echo "$BASIC_DIRECTORY"
 
-for ITEM in $BASIC_DIRECTORY; do
+for ITEM in "$BASIC_DIRECTORY"; do
     echo "Проверка: $ITEM"
     if [ -d "$ITEM" ]; then
         $SUDO chmod -R "$ACCESS" "$ITEM"
