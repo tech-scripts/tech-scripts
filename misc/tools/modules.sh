b26585e2988163ae07ef67983becc062630fc524
tech-scripts <techscriptsgithub@gmail.com>
2025-05-03 00:45:49 +0500

    Update modules.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

--- misc/tools/modules.sh
+++ misc/tools/modules.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 
-# Determine user directory based on write access to /tmp
 USER_DIR=$( [ -w /tmp ] && echo "" || echo "$HOME" )
-
-# Source external script
 source "$USER_DIR/etc/tech-scripts/source.sh"
 
-# Function to check if a module is loaded or accessible
 check_module() {
     local name="$1"
     local display_name="$2"
@@ -14,25 +10,20 @@ check_module() {
     local module_dir="/lib/modules/$(uname -r)/kernel/$name"
     local access_status="✗"
 
-    # Check if the module is loaded or accessible
     if lsmod | grep -q "$name" || [ -d "$module_dir" ] && [ -r "$module_dir" ] && [ -w "$module_dir" ] && [ -x "$module_dir" ] || modprobe "$name" &> /dev/null; then
         access_status="✓"
     fi
 
-    # Check if the path exists
     [ -e "$path" ] && access_status="✓"
-
     results+=("$display_name: $access_status")
 }
 
-# Display menu and get user choice
 choice=$(whiptail --title "$NEED_TITLE" --menu "$NEED_MENU_TITLE" 12 40 4 \
 "1" "Docker" \
 "2" "Podman" \
 "3" "LXC" \
 "4" "$NEED_ALL" 3>&1 1>&2 2>&3)
 
-# Define mandatory and optional modules based on user choice
 case $choice in
     1) mandatory_modules=("overlay" "br_netfilter" "ip_tables" "ip6_tables" "nf_nat"); optional_modules=("fuse" "nfs" "cifs" "seccomp" "audit" "lockdown"); echo -e "\n$NEED_CHECKING_MODULES Docker:" ;;
     2) mandatory_modules=("overlay" "br_netfilter" "ip_tables" "ip6_tables" "nf_nat" "cgroup" "namespace"); optional_modules=("fuse" "nfs" "cifs" "seccomp" "audit" "lockdown"); echo -e "\n$NEED_CHECKING_MODULES Podman:" ;;
@@ -43,8 +34,7 @@ esac
 
 results=()
 
-# Display results
-echo -e "\n$NEED_SYSTEM_MODULES"
+echo -e "\nСистемные модули"
 for module in "${mandatory_modules[@]}"; do
     case $module in
         "overlay") check_module "overlay" "OverlayFS" "/sys/module/overlay" ;;
@@ -57,7 +47,7 @@ for module in "${mandatory_modules[@]}"; do
     esac
 done
 
-echo -e "\n$NEED_FILESYSTEMS"
+echo -e "\nФайловые системы"
 for module in "${optional_modules[@]}"; do
     case $module in
         "fuse") check_module "fuse" "FUSE" "/sys/fs/fuse" ;;
@@ -66,30 +56,31 @@ for module in "${optional_modules[@]}"; do
     esac
 done
 
-echo -e "\n$NEED_NETWORK_MODULES"
-check_module "ip_forward" "Network namespaces" "/proc/sys/net/ipv4/ip_forward"
-check_module "capabilities" "Capabilities" "/proc/sys/kernel/cap_last_cap"
+if [[ "$choice" == "4" ]]; then
+    echo -e "\nСетевые модули"
+    check_module "ip_forward" "Network namespaces" "/proc/sys/net/ipv4/ip_forward"
+    check_module "capabilities" "Capabilities" "/proc/sys/kernel/cap_last_cap"
 
-echo -e "\n$NEED_IPC"
-check_module "shmmax" "Shared Memory" "/proc/sys/kernel/shmmax"
-check_module "msgmax" "Message Queues" "/proc/sys/kernel/msgmax"
-check_module "sem" "Semaphores" "/proc/sys/kernel/sem"
+    echo -e "\nIPC (Межпроцессное взаимодействие)"
+    check_module "shmmax" "Shared Memory" "/proc/sys/kernel/shmmax"
+    check_module "msgmax" "Message Queues" "/proc/sys/kernel/msgmax"
+    check_module "sem" "Semaphores" "/proc/sys/kernel/sem"
 
-echo -e "\n$NEED_SECURITY"
-check_module "audit" "Kernel Auditing" "/proc/sys/kernel/audit"
-check_module "lockdown" "Kernel Lockdown" "/proc/sys/kernel/lockdown"
-check_module "cgroups" "cgroups" "/proc/cgroups"
+    echo -e "\nБезопасность"
+    check_module "audit" "Kernel Auditing" "/proc/sys/kernel/audit"
+    check_module "lockdown" "Kernel Lockdown" "/proc/sys/kernel/lockdown"
+    check_module "cgroups" "cgroups" "/proc/cgroups"
 
-echo -e "\n$NEED_UNIQUE_IDS"
-check_module "hostname" "UTS namespace" "/proc/sys/kernel/hostname"
-check_module "keys" "Keyrings" "/proc/sys/kernel/keys"
+    echo -e "\nУникальные идентификаторы"
+    check_module "hostname" "UTS namespace" "/proc/sys/kernel/hostname"
+    check_module "keys" "Keyrings" "/proc/sys/kernel/keys"
+fi
 
-# Display results without double checkmarks or crosses
 echo -e "\nРезультаты проверки модулей:"
 for result in "${results[@]}"; do
     if [[ "$result" == *"✓"* ]]; then
-        echo -e "\e[37m$result \e[32m✓\e[0m"  # Green checkmark for success
+        echo -e "\e[37m$result \e[32m✓\e[0m"
     else
-        echo -e "\e[37m$result \e[31m✗\e[0m"  # Red cross for failure
+        echo -e "\e[37m$result \e[31m✗\e[0m"
     fi
 done
