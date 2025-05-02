466cc6226c7391cfa612d61adb3e0e453a9808fc
tech-scripts <techscriptsgithub@gmail.com>
2025-05-02 21:39:51 +0500

    Update memory.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

--- misc/tools/benchmark/memory.sh
+++ misc/tools/benchmark/memory.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-source /etc/tech-scripts/localization.sh
-source /etc/tech-scripts/variables.sh
+[ -w /tmp ] && USER_DIR="" || USER_DIR="~"
+
+source $USER_DIR/etc/tech-scripts/source.sh
 
 install_npm() {
     if command -v apt &>/dev/null; then
