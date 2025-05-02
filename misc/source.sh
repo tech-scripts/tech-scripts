f6601806c87cfe38ac4d1eaac49571db421039ae
tech-scripts <techscriptsgithub@gmail.com>
2025-05-02 21:16:51 +0500

    Create source.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

---
+++ misc/source.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+[ -w /tmp ] && USER_DIR="" || USER_DIR="~"
+
+source $USER_DIR/etc/tech-scripts/localization.sh
+source $USER_DIR/etc/tech-scripts/variables.sh
+source $USER_DIR/etc/tech-scripts/functions.sh
