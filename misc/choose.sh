395ee751f9f6796b158062dafae2d173ba888058
tech-scripts <techscriptsgithub@gmail.com>
2025-05-02 23:16:13 +0500

    Update choose.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

--- misc/choose.sh
+++ misc/choose.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-for script in /tmp/tech-scripts/misc/tech/{tech,lang,access,editor}.sh; do
+[ -w /tmp ] && USER_DIR="" || USER_DIR=$HOME
+
+for script in $USER_DIR/tmp/tech-scripts/misc/tech/{tech,lang,access,editor}.sh; do
     chmod +x "$script" && "$script"
 done
