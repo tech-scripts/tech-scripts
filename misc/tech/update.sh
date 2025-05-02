5e03b3fb9418e5da25bbd38dfe54a379b97b3e83
tech-scripts <techscriptsgithub@gmail.com>
2025-05-02 21:53:18 +0500

    Update update.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

--- misc/tech/update.sh
+++ misc/tech/update.sh
@@ -7,11 +7,11 @@ cat << 'EOF' > /tmp/update.sh
 
 source $USER_DIR/etc/tech-scripts/source.sh
 
-cd /tmp
+cd $USER_DIR/tmp
 $SUDO rm -rf /tmp/tech-scripts
-git clone --depth 1 https://github.com/tech-scripts/tech-scripts.git /tmp/tech-scripts
+git clone --depth 1 https://github.com/tech-scripts/tech-scripts.git $USER_DIR/tmp/tech-scripts
 $SUDO rm -rf /usr/local/bin/tech
-cd "/tmp/tech-scripts/misc/tech"
+cd $USER_DIR/tmp/tech-scripts/misc/tech
 chmod +x tech.sh
 ./tech.sh
 $SUDO cp -f $USER_DIR/tmp/tech-scripts/misc/localization.sh $USER_DIR/etc/tech-scripts/
@@ -28,5 +28,5 @@ fi
 echo ""
 EOF
 chmod +x /tmp/update.sh
-/tmp/update.sh
-#rm -f /tmp/update.sh
+$USER_DIR/tmp/update.sh
+rm -f /tmp/update.sh
