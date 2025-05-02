a6ef7ac126dd6c1e63496154aad5b75bcf1401c1
tech-scripts <techscriptsgithub@gmail.com>
2025-05-02 21:41:20 +0500

    Update swap.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

--- misc/tools/swap.sh
+++ misc/tools/swap.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-source /etc/tech-scripts/localization.sh
-source /etc/tech-scripts/variables.sh
+[ -w /tmp ] && USER_DIR="" || USER_DIR="~"
+
+source $USER_DIR/etc/tech-scripts/source.sh
 
 is_valid_size() {
     [[ "$1" =~ ^[0-9]+[GgMmKk]$ ]]
