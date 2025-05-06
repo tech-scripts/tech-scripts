1b4091fa09367b7b50952c84f9ac11ba1b1bdcb2
tech-scripts <techscriptsgithub@gmail.com>
2025-05-06 23:26:37 +0500

    Update functions.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

--- misc/functions.sh
+++ misc/functions.sh
@@ -4,6 +4,11 @@ change_directory_permissions() {
     [ -n "$BASIC_DIRECTORY" ] && IFS=' ' read -r -a directories <<< "$BASIC_DIRECTORY"
 
     for dir in "${directories[@]}"; do
-        [ -d "$dir" ] && [ "$(stat -c "%a" "$dir")" != "$ACCESS" ] && $SUDO chmod -R "$ACCESS" "$dir"
+        if [ -n "$dir" ] && [ -d "$dir" ]; then
+            if [ "$(stat -c "%a" "$dir")" != "$ACCESS" ]; then
+                echo "$ACCESS $dir"
+                $SUDO chmod -R "$ACCESS" "$dir"
+            fi
+        fi
     done
 }
