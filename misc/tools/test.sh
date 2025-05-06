0af13a96a571b76e2bbfbfc5f0b7b572fb681fcf
tech-scripts <techscriptsgithub@gmail.com>
2025-05-06 21:04:59 +0500

    Create test.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

---
+++ misc/tools/test.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+[ -n "\$BASIC_DIRECTORY" ] && IFS=' ' read -r -a directories <<< "\$BASIC_DIRECTORY"
+
+for dir in "\${directories[@]}"; do
+    [ -d "\$dir" ] && [ "\$(stat -c "%a" "\$dir")" != "\$ACCESS" ] && \$SUDO chmod -R "\$ACCESS" "\$dir"
+done
