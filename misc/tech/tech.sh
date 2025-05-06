8d150b5697318b9712147cfd8e1d50e0a4f94ced
tech-scripts <techscriptsgithub@gmail.com>
2025-05-06 21:35:17 +0500

    Update tech.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

--- misc/tech/tech.sh
+++ misc/tech/tech.sh
@@ -25,7 +25,7 @@ BASIC_DIRECTORY=\$(echo "\$BASIC_DIRECTORY" | tr -s ' ')
 [ -n "\$BASIC_DIRECTORY" ] && IFS=' ' read -r -a directories <<< "\$BASIC_DIRECTORY"
 
 for dir in "\${directories[@]}"; do
-    [ -d "\$dir" ] && [ "\$(stat -c "%a" "\$dir")" != "\$ACCESS" ] && \$SUDO chmod -R "\$ACCESS" "\$dir"
+    [ -d "\$dir" ] && [ "\$(stat -c "%a" "\$dir")" != "\$ACCESS" ] && echo "$ACCESS $dir" && \$SUDO chmod -R "\$ACCESS" "\$dir"
 done
 
 run_script() {
