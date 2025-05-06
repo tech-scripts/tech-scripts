4e2207fd916ebdb4a4a1085a64111fcb0d73d581
tech-scripts <techscriptsgithub@gmail.com>
2025-05-06 23:30:42 +0500

    Update functions.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

--- misc/functions.sh
+++ misc/functions.sh
@@ -2,7 +2,7 @@
 
 change_directory_permissions() {
     [ -n "$BASIC_DIRECTORY" ] && IFS=' ' read -r -a directories <<< "$BASIC_DIRECTORY"
-
+    echo "777"
     for dir in "${directories[@]}"; do
         if [ -n "$dir" ] && [ -d "$dir" ]; then
             if [ "$(stat -c "%a" "$dir")" != "$ACCESS" ]; then
