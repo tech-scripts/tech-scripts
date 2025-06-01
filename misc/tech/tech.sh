e7cf6652e392351c1b76c432a0a755bc3a8956ae
tech-scripts <techscriptsgithub@gmail.com>
2025-06-01 19:47:14 +0500

    Update tech.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

--- misc/tech/tech.sh
+++ misc/tech/tech.sh
@@ -60,7 +60,7 @@ if [ -f $TECH_COMMAND_DIR/tech ]; then
         $SUDO rm $TECH_COMMAND_DIR/tech
         $SUDO tee $TECH_COMMAND_DIR/tech > /dev/null <<< "$TECH_SCRIPT"
         $SUDO chmod +x $TECH_COMMAND_DIR/tech
-        $SUDO hash -d tech
+        hash -d tech
         echo " "
         echo "$MSG_UPDATED_TECH"
         echo " "
@@ -79,5 +79,5 @@ if [ -f $TECH_COMMAND_DIR/tech ]; then
 else
     $SUDO tee $TECH_COMMAND_DIR/tech > /dev/null <<< "$TECH_SCRIPT"
     $SUDO chmod +x $TECH_COMMAND_DIR/tech
-    $SUDO hash -d tech
+    hash -d tech
 fi
