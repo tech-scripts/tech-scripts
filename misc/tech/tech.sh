32985f5a6a5332df04ca04323eb4e62a0f5cf5d4
tech-scripts <techscriptsgithub@gmail.com>
2025-06-10 00:41:39 +0500

    Update tech.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

--- misc/tech/tech.sh
+++ misc/tech/tech.sh
@@ -44,6 +44,7 @@ case "\$combined_args" in
     "autoupdate") run_script "tools" "autoupdate.sh" ;;
     "grub") run_script "tools" "grub.sh" ;;
     "kernel") run_script "tools" "kernel.sh" ;;
+    "ports") run_script "tools" "ports.sh" ;;
     "ssh") run_script "tools" "ssh.sh" ;;
     "startup") run_script "tools" "startup.sh" ;;
     "swap") run_script "tools" "swap.sh" ;;
