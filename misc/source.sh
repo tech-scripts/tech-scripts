a5a8f1b9b0252e6362e7b8232c28fa0e3fa84e1c
tech-scripts <techscriptsgithub@gmail.com>
2025-11-02 12:50:28 +0500

    Update source.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

--- misc/source.sh
+++ misc/source.sh
@@ -2,6 +2,6 @@
 
 [ -w /tmp ] && USER_DIR="" || USER_DIR=$HOME
 
-source $USER_DIR/etc/tech-scripts/localization.sh
-source $USER_DIR/etc/tech-scripts/variables.sh
-source $USER_DIR/etc/tech-scripts/functions.sh
+source $USER_DIR/opt/tech-scripts/localization.sh
+source $USER_DIR/opt/tech-scripts/variables.sh
+source $USER_DIR/opt/tech-scripts/functions.sh
