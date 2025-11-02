42a00960748ab405f49ae75744f981c601f1188f
tech-scripts <techscriptsgithub@gmail.com>
2025-11-02 12:53:16 +0500

    Update root.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

--- misc/tools/root.sh
+++ misc/tools/root.sh
@@ -2,7 +2,7 @@
 
 [ -w /tmp ] && USER_DIR="" || USER_DIR=$HOME
 
-source $USER_DIR/etc/tech-scripts/source.sh
+source $USER_DIR/opt/tech-scripts/source.sh
 
 check_dependencies() {
     if ! command -v whiptail &> /dev/null; then
