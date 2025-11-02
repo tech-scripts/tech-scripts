f46705dc14e8285087c8334d74c57a6a981b0edc
tech-scripts <techscriptsgithub@gmail.com>
2025-11-02 12:51:21 +0500

    Update system.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

--- misc/tools/system.sh
+++ misc/tools/system.sh
@@ -2,7 +2,7 @@
 
 [ -w /tmp ] && USER_DIR="" || USER_DIR=$HOME
 
-source $USER_DIR/etc/tech-scripts/source.sh
+source $USER_DIR/opt/tech-scripts/source.sh
 
 show_temperature_info() {
     if ! command -v sensors &>/dev/null; then
