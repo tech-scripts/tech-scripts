eb9448f5ac2a9fa3f988cc4bb389201dd4bdd826
tech-scripts <techscriptsgithub@gmail.com>
2026-07-07 19:14:36 +0500

    Update start.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

--- misc/start.sh
+++ misc/start.sh
@@ -212,16 +212,14 @@ BASIC_DIRECTORY=$(echo "$BASIC_DIRECTORY" | tr -s ' ')
 
 [ -n "$BASIC_DIRECTORY" ] && IFS=' ' read -r -a directories <<< "$BASIC_DIRECTORY"
 
-getent group tech > /dev/null 2>&1 || { command -v groupadd > /dev/null 2>&1 && $SUDO groupadd tech; }
-
 for dir in "${directories[@]}"; do
   [ -n "$dir" ] && [ -e "$dir" ] || continue
-  if [ "$(stat -c "%a" "$dir")" != "$ACCESS" ] || [ "$(stat -c "%G" "$dir")" != "tech" ]; then
-    CMD="chmod -R $ACCESS $dir; getent group tech > /dev/null 2>&1 && chgrp -R tech $dir"
-    if ! chgrp -R tech "$dir" 2>/dev/null; then
+  if [ "$(stat -c "%a" "$dir")" != "$ACCESS" ]; then
+    CMD="chmod -R $ACCESS $dir"
+    if ! chmod -R "$ACCESS" "$dir" 2>/dev/null; then
       $SUDO bash -c "$CMD"
     else
-      bash -c "$CMD"
+      sh -c "$CMD"
     fi
   fi
 done
