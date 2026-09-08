dc46b2c9b4d5fe5fa99e53e12dd8368e7801e9cb
tech-scripts <techscriptsgithub@gmail.com>
2026-09-08 14:44:27 +0500

    Update tech.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

--- misc/tech/tech.sh
+++ misc/tech/tech.sh
@@ -1,3 +1,8 @@
+#!/usr/bin/env bash
+
+[ "${HOME##*/}" = ".suroot" ] && export HOME="${HOME%/*}"
+[ -w /tmp ] && USER_DIR="" || USER_DIR=$HOME
+SUDO=$(env | grep -qi TERMUX && echo "" || command -v sudo 2>/dev/null)
 source $USER_DIR/opt/tech-scripts/source.sh
 
 TECH_SCRIPT=$(cat <<'TECHSCRIPT'
