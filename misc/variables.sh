fa7fcbac3382fece8e6e1a7c5a14db672d3c87a6
tech-scripts <techscriptsgithub@gmail.com>
2026-09-08 15:12:34 +0500

    Update variables.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

--- misc/variables.sh
+++ misc/variables.sh
@@ -8,7 +8,13 @@ LANGUAGE=$(grep '^lang:' $USER_DIR/opt/tech-scripts/choose.conf | cut -d ' ' -f 
 EDITOR=$(grep '^editor:' $USER_DIR/opt/tech-scripts/choose.conf | cut -d ' ' -f 2)
 ACCESS=$(grep '^access:' $USER_DIR/opt/tech-scripts/choose.conf | cut -d ' ' -f 2)
 HIDE=$(grep '^hide:' $USER_DIR/opt/tech-scripts/choose.conf | cut -d ' ' -f 2)
-TECH_COMMAND_DIR=$USER_DIR/usr/local/bin
+if env | grep -qi TERMUX; then
+    TECH_COMMAND_DIR="$PREFIX/bin"
+    USER_DIR="$PREFIX"
+else
+    TECH_COMMAND_DIR=/usr/local/bin
+    [ -w /tmp ] && USER_DIR="" || USER_DIR=$HOME
+fi
 CONFIG_FILE="$USER_DIR/opt/tech-scripts/choose.conf"
 SCRIPT_DIR_SSH="$USER_DIR/usr/local/tech-scripts"
 CONFIG_FILE_SSH="$USER_DIR/opt/tech-scripts/alert.conf"
