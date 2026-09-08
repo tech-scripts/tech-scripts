b57ca65b0bcbc04026ef13dd22102da736c0feb9
tech-scripts <techscriptsgithub@gmail.com>
2026-09-08 15:17:25 +0500

    Update variables.sh
    Signed-off-by: tech-scripts <techscriptsgithub@gmail.com>

--- misc/variables.sh
+++ misc/variables.sh
@@ -2,20 +2,13 @@
 
 [ -w /tmp ] && USER_DIR="" || USER_DIR=$HOME
 
-if env | grep -qi TERMUX; then
-    TECH_COMMAND_DIR="$PREFIX/bin"
-    USER_DIR="$PREFIX"
-else
-    TECH_COMMAND_DIR=/usr/local/bin
-    [ -w /tmp ] && USER_DIR="" || USER_DIR=$HOME
-fi
-
 SUDO=$(env | grep -qi TERMUX && echo "" || command -v sudo 2>/dev/null)
 CURRENT_DIR=$(pwd)
 LANGUAGE=$(grep '^lang:' $USER_DIR/opt/tech-scripts/choose.conf | cut -d ' ' -f 2)
 EDITOR=$(grep '^editor:' $USER_DIR/opt/tech-scripts/choose.conf | cut -d ' ' -f 2)
 ACCESS=$(grep '^access:' $USER_DIR/opt/tech-scripts/choose.conf | cut -d ' ' -f 2)
 HIDE=$(grep '^hide:' $USER_DIR/opt/tech-scripts/choose.conf | cut -d ' ' -f 2)
+TECH_COMMAND_DIR="$USER_DIR/usr/local/bin"
 CONFIG_FILE="$USER_DIR/opt/tech-scripts/choose.conf"
 SCRIPT_DIR_SSH="$USER_DIR/usr/local/tech-scripts"
 CONFIG_FILE_SSH="$USER_DIR/opt/tech-scripts/alert.conf"
